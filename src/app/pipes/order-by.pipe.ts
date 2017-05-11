a1f3a2cb00d546af810f3be8a66669282c5088f7
LBaccillere <lbaccillere@outlook.com>
2017-05-11 13:25:34 -0300

    Corrección en el order pipe si el elemento no esta definido.

--- src/app/pipes/order-by.pipe.ts
+++ src/app/pipes/order-by.pipe.ts
@@ -4,22 +4,28 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'orderBy',
   pure: false
 })
+
 export class OrderByPipe implements PipeTransform {
 
   static _orderByComparator(a:any, b:any):number{
-        
-      if((isNaN(parseFloat(a)) || !isFinite(a)) || (isNaN(parseFloat(b)) || !isFinite(b))){
+        if(a === undefined) {
+            a="";
+        }
+        if(b === undefined) {
+            b="";
+        }
+        if((isNaN(parseFloat(a)) || !isFinite(a)) || (isNaN(parseFloat(b)) || !isFinite(b))){
         //Isn't a number so lowercase the string to properly compare
         if(a.toLowerCase() < b.toLowerCase()) return -1;
         if(a.toLowerCase() > b.toLowerCase()) return 1;
-      }
-      else{
+    }
+    else{
         //Parse strings as numbers to compare properly
         if(parseFloat(a) < parseFloat(b)) return -1;
         if(parseFloat(a) > parseFloat(b)) return 1;
-      }
-    
-      return 0; //equal each other
+    }
+
+    return 0; //equal each other
     }
 
     transform(input:any, [config = '+']): any{
