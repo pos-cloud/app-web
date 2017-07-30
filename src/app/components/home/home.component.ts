927658dce656f3473dfd7d8492447b2595b6e88d
franco <franco@debian>
2017-07-30 13:55:35 -0300

    Home vacio

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Observable';
 
 
 @Component({
@@ -12,16 +10,10 @@ import { Observable } from 'rxjs/Observable';
 export class HomeComponent implements OnInit {
 
   public userType: string;
-  public online: Observable<boolean>;
   
   constructor(
     public _router: Router
   ) { 
-    this.online = Observable.merge(
-      Observable.of(navigator.onLine),
-      Observable.fromEvent(window, 'online').mapTo(true),
-      Observable.fromEvent(window, 'offline').mapTo(false)
-    )
   }
 
   ngOnInit() {
