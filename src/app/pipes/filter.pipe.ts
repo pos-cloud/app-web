de9b4776cc3ada32935eb2afa730f2c32b3201c9
LLBaccillere <lbaccillere@outlook.com>
2017-11-06 01:43:17 -0300

    Corrección al filtrar transacciónes. Cambios de rutas. Trabajando sobre los filtros de fechas.

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Pipe({
   name: 'filter'
@@ -9,7 +10,7 @@ export class FilterPipe implements PipeTransform {
   result: any;
   strVal: string = "";
   strArg: string = "";
-//   terms: string[];
+  terms: string[];
   valueAux: any[];
 
   transform(value: any[], term: string, property: string, subobject?:string): any {
@@ -20,16 +21,50 @@ export class FilterPipe implements PipeTransform {
     } else {
 
         // this.terms = term.split(';');
-        // if (this.terms.length == 1){    //nos permite hacer busqueda por varios campos en un solo inpu seguido de ';'
-        //      this.valueAux = value;
-        // }
+            // if (this.terms.length == 1){    //nos permite hacer busqueda por varios campos en un solo inpu seguido de ';'
+            //      this.valueAux = value;
+            // }
+
+        if(property && property.toLowerCase().includes('date'.toLowerCase())) {
+            let terms = term.split('/');
+            console.log("antes" + terms);
+            let final = "";
+            let aux2 = "";
+            // 04/11/2017
+            // /04
+            // /04
+            for(let i=0; i < terms.length; i++) {
+                if(terms[i+1]) {
+                    if(i == 0) {
+                        final = "/" + terms[i];
+                    }
+                    if(i == 1) {
+                        final += "/" + terms[i];
+                    }
+                    
+                } else {
+                    if(final == "") {
+                        final = terms[i];
+                    } else {
+                        if(i == 2) {
+                            final += terms[i];
+                        }
+                    }
+                }
+            }
+            term = final;
+            // this.terms = term.split(';');
+            // if (this.terms.length == 1){    //nos permite hacer busqueda por varios campos en un solo inpu seguido de ';'
+            //      this.valueAux = value;
+            // }
+            console.log("despues" +term);
+        }
 
         this.valueAux = value;
 
         this.result = this.valueAux.filter(item => {
             
             for (let key in item) {
-
                 if(subobject !== undefined) {
 
                     if(key === subobject) {  //rechaza buscar por _id, y verifica si cual es la propiedad del objeto
