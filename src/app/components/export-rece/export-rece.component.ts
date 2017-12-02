70ba7a57923243f34410e1ae9715d850fee73961
Franco-PC\Franco <androettofranco@gmail.com>
2017-12-02 11:39:28 -0300

    Actualizacion RECE

--- src/app/components/export-rece/export-rece.component.ts
+++ src/app/components/export-rece/export-rece.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { NgbModal, NgbActiveModal, NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
+import * as moment from 'moment';
+import 'moment/locale/es';
 
 import { TransactionService } from './../../services/transaction.service';
 
@@ -20,18 +22,32 @@ export class ExportReceComponent implements OnInit {
   public loading: boolean = false;
   public focusEvent = new EventEmitter<boolean>();
   public modelToImport: Array<String>;
+  public months = ["01", "02", "03", "04", "05", "06", "07", "08", "09","10","11","12"];
 
   public formErrors = {
     'destination': '',
-    'period': ''
+    'month': '',
+    'year' : '',
+    'pointofsale': ''
   };
 
   public validationMessages = {
     'destination' : {
+      'required':     'Este campo es requerido.',
+      'pattern' :     'El archivo debe terminar en .txt'
+    },
+    'month' : {
       'required':     'Este campo es requerido.'
     },
-    'period' : {
-      'required':     'Este campo es requerido.'
+    'year' : {
+      'required':     'Este campo es requerido.',
+      'minlength':      'El año debe contener 4 digitos.',
+      'maxlength':      'El año debe contener 4 digitos.',
+    },
+    'pointofsale' : {
+      'required':     'Este campo es requerido',
+      'minlength':      'El punto de venta debe contener 4 digitos.',
+      'maxlength':      'El punto de venta debe contener 4 digitos.',
     }
   };
 
@@ -47,18 +63,32 @@ export class ExportReceComponent implements OnInit {
   ngOnInit() {
     let pathLocation: string[] = this._router.url.split('/');
     this.buildForm();
-    this.exportReceForm.setValue({
-      'destination': "",
-      'period': ""
-    });
   }
 
   public buildForm(): void {
     this.exportReceForm = this._fb.group({
-      'destination': "",
-      'period': ""
+      'destination': ["C:/temp/nombre-archivo.txt", [
+        Validators.required,
+        Validators.pattern(".*\.txt"),
+        ]
+      ],
+      'month': [moment().format("MM"), [
+        Validators.required
+        ]
+      ],
+      'year': [moment().format("YYYY"), [
+        Validators.required,
+        Validators.maxLength(4),
+        Validators.minLength(4),
+        ]
+      ],
+      'pointofsale':[, [
+        Validators.required,
+        Validators.maxLength(4),
+        Validators.minLength(4),
+        ]
+      ],
     });
-
     this.exportReceForm.valueChanges
       .subscribe(data => this.onValueChanged(data));
 
@@ -89,12 +119,11 @@ export class ExportReceComponent implements OnInit {
     this.modelToImport = this.exportReceForm.value;
     this._serviceTransaction.getFileAfip(this.modelToImport).subscribe(
     result => {
-        if (!result) {
+        if (result.message !== "OK") {
           this.showMessage(result.message, "info", true); 
           this.loading = false;
         } else {
           this.showMessage("El archivo se genero correctamente.", "success", false);
-          this.activeModal.close('save_close');
         }
         this.loading = false;
       },
