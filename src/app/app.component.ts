1443f124070e40caf10617026a9e96da15542556
unknown <androettofranco@gmail.com>
2018-10-08 20:52:39 -0300

    Add fantasy name in company

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,7 +58,7 @@ export class AppComponent implements OnInit {
     if (config.pathBackup) { Config.setConfigToBackup(config.pathBackup, config.pathMongo, config.backupTime); }
     if (config.emailAccount) { Config.setConfigEmail(config.emailAccount, config.emailPassword) }
     if (config.companyName) { Config.setConfigCompany(config.companyPicture, config.companyName, config.companyCUIT, config.companyAddress, config.companyPhone,
-                                                    config.companyVatCondition, config.companyStartOfActivity, config.companyGrossIncome, config.footerInvoice);
+                                                    config.companyVatCondition, config.companyStartOfActivity, config.companyGrossIncome, config.footerInvoice, config.companyFantasyName);
     }
     if (config.modules) {
       Config.setModules(config.modules);
