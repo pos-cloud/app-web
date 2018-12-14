af347f39a39f7c472f54d451bd2c02a19ebdcbcf
unknown <androettofranco@gmail.com>
2018-12-14 14:48:57 -0300

    Auditoria Make

--- src/app/models/make.ts
+++ src/app/models/make.ts
@@ -1,8 +1,16 @@
+import { User } from './user';
+
+import * as moment from 'moment';
+import 'moment/locale/es';
+
 export class Make {
 	
 	public _id: string;
 	public description: string = '';
-	public visibleSale: boolean = false;
+	public visibleSale: boolean = false;  public creationUser: User;
+	public creationDate: string = moment().format('YYYY-MM-DDTHH:mm:ssZ');
+	public updateUser: User;
+	public updateDate: string = moment().format('YYYY-MM-DDTHH:mm:ssZ');
 
 	constructor () {}
 }
