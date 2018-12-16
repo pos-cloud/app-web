32e084d0c42552f45c16f9b5f311b113dba2ad58
unknown <androettofranco@gmail.com>
2018-12-16 00:29:49 -0300

    Auditoria de Company Group

--- src/app/models/company-group.ts
+++ src/app/models/company-group.ts
@@ -1,7 +1,17 @@
+import { User } from './user';
+
+import * as moment from 'moment';
+import 'moment/locale/es';
+
 export class CompanyGroup {
 	
 	public _id: string;
 	public description: string = '';
+	public creationUser: User;
+	public creationDate: string = moment().format('YYYY-MM-DDTHH:mm:ssZ');
+	public updateUser: User;
+	public updateDate: string = moment().format('YYYY-MM-DDTHH:mm:ssZ');
+  
 
 	constructor () {}
 }
