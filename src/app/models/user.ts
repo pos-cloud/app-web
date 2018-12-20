76a3fbe56a30d7d6e9ad0f2785eafcdb2422df0e
Lucas Baccillere <lbaccillereoo@outlook.com>
2018-12-19 22:26:56 -0300

    Aumentar los minitos de sesion.

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,13 +1,13 @@
 import { Employee } from './employee';
 
 export class User {
-	
+
 	public _id: string;
 	public name: string;
 	public password: string;
 	public state: UserState;
 	public token: string;
-	public tokenExpiration: number = 1440;
+  public tokenExpiration: number = 9999;
 	public employee: Employee = null;
 
 	constructor () {}
