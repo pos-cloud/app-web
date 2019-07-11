8b3be5b154f4f90572097551d41d38680bba8e50
unknown <androettofranco@gmail.com>
2019-07-11 00:11:12 -0300

    vuelve como estaba antes round number

--- src/app/pipes/round-number.pipe.ts
+++ src/app/pipes/round-number.pipe.ts
@@ -11,7 +11,7 @@ export class RoundNumberPipe implements PipeTransform {
           case 1:
             return Math.round(value * 10) / 10;
           case 2:
-            return (Math.round(value * 100) / 100).toFixed(2);
+            return Math.round(value * 100) / 100;
           case 3:
             return Math.round(value * 1000) / 1000;
           case 4:
