cdce3c8ab6b33a35fce9f311dbb3290c64503df1
LLBaccillere <lbaccillere@outlook.com>
2019-07-12 04:55:12 -0300

    Corregir calculo de financiacion al cambiar fecha o montos. El articulo recargo no esta sumando el iva a la transaccion. Arreglar producto descuento para los medios de pago

--- src/app/components/list-movements-of-cashes/list-movements-of-cashes.component.ts
+++ src/app/components/list-movements-of-cashes/list-movements-of-cashes.component.ts
@@ -17,12 +17,12 @@ import { ViewTransactionComponent } from '../view-transaction/view-transaction.c
 
 export class ListMovementOfCashesComponent implements OnInit {
 
-  @Input() userType : string;
   @Input() transactionAmount : number;
   public movementsOfCashes: MovementOfCash[] = new Array();
+  public movementsOfCashesSelected: MovementOfCash[] = new Array();
   public areMovementOfCashesEmpty = true;
+  public userType : string;
   public alertMessage = '';
-  //public userType: string;
   public orderTerm: string[] = ['-expirationDate'];
   public propertyTerm: string;
   public areFiltersVisible = false;
@@ -79,19 +79,12 @@ export class ListMovementOfCashesComponent implements OnInit {
   ngOnInit(): void {
 
     this.pathLocation = this._router.url.split('/');
-    if(!this.userType){
-      this.userType = this.pathLocation[1];
-    }
+    this.userType = this.pathLocation[1];
     this.transactionMovement = this.pathLocation[2].charAt(0).toUpperCase() + this.pathLocation[2].slice(1);
-    if(this.userType === "admin"){
-      this.getMovementOfCashes();
-    } else {
-      this.getMovementOfCashesV2();
-    }
-    
+    this.getMovementOfCashes();
   }
 
-  public getMovementOfCashesV2() : void {
+  public getMovementOfCashes() : void {
     
     this.loading = true;
 
@@ -107,33 +100,66 @@ export class ListMovementOfCashesComponent implements OnInit {
       }
     }
 
-    match += `"operationType": { "$ne": "D" }, "statusCheck" : "Disponible" }`;
+    if(this.userType === 'admin') {
+      match += `"operationType": { "$ne": "D" }, "transaction.type.transactionMovement" : "${this.transactionMovement}" }`;
+    } else {
+      match += `"operationType": { "$ne": "D" }, "statusCheck" : "${StatusCheck.Available}" }`;
+    }
 
     match = JSON.parse(match);
     
+    let project = {};
     // CAMPOS A TRAER
-    let project = {
-      "_id" : 1,
-      "number": 1,
-      "bank._id" : 1 ,
-      "bank.name" : { $toString : '$bank.name'} ,
-      "amountPaid" :{ $toString : '$amountPaid'} ,
-      "operationType": 1,
-      "expirationDate": { $dateToString: { date: "$expirationDate", format: "%d/%m/%Y", timezone: "-03:00" }},
-      "transaction._id":1,
-      "transaction.state" : 1,
-      "transaction.type.name" : 1,
-      "transaction.type.transactionMovement" : 1,
-      "date": 1,
-      "statusCheck": 1,
-      "titular" : 1,
-      "receiver" : 1,
-      "quota" : 1 ,
-      "type" : 1,
-      "deliveredBy" : 1,
-      "CUIT" : 1,
-      "observation" : 1
-    };
+    if(this.userType === 'admin') {
+      project = {
+        "_id" : 1,
+        "transaction.endDate": { $dateToString : { date: "$transaction.endDate", format: "%d/%m/%Y", timezone: "-03:00"}},
+        "transaction.cashBox.number" : { $toString : "$transaction.cashBox.number"},
+        "quota" :1 ,
+        "discount": 1,
+        "number" : 1,
+        "statusCheck" : 1,
+        "observation" : 1,
+        "bank._id" : 1 ,
+        "bank.name" : { $toString : '$bank.name'} , 
+        "amountPaid" :{ $toString : '$amountPaid'} ,
+        "operationType": 1,
+        "expirationDate": { $dateToString: { date: "$expirationDate", format: "%d/%m/%Y", timezone: "-03:00" }},
+        "transaction._id":1,
+        "transaction.state" : 1,
+        "transaction.type.name" : 1,
+        "transaction.type.transactionMovement" : 1,
+        "date": 1,
+        "titular" : 1,
+        "receiver" : 1,
+        "type" : 1,
+        "deliveredBy" : 1,
+        "CUIT" : 1,
+      };
+    } else {
+      project = {
+        "_id" : 1,
+        "number": 1,
+        "bank._id" : 1 ,
+        "bank.name" : { $toString : '$bank.name'} ,
+        "amountPaid" :{ $toString : '$amountPaid'} ,
+        "operationType": 1,
+        "expirationDate": { $dateToString: { date: "$expirationDate", format: "%d/%m/%Y", timezone: "-03:00" }},
+        "transaction._id":1,
+        "transaction.state" : 1,
+        "transaction.type.name" : 1,
+        "transaction.type.transactionMovement" : 1,
+        "date": 1,
+        "statusCheck": 1,
+        "titular" : 1,
+        "receiver" : 1,
+        "quota" : 1 ,
+        "type" : 1,
+        "deliveredBy" : 1,
+        "CUIT" : 1,
+        "observation" : 1
+      };
+    }
 
     // AGRUPAMOS EL RESULTADO
     let group = {
@@ -149,7 +175,6 @@ export class ListMovementOfCashesComponent implements OnInit {
     let skip = !isNaN(page * this.itemsPerPage) ?
             (page * this.itemsPerPage) :
                 0 // SKIP
-
     this._movementOfCashService.getMovementsOfCashesV2(
         project, // PROJECT
         match, // MATCH
@@ -180,133 +205,18 @@ export class ListMovementOfCashesComponent implements OnInit {
 
   public pageChange(page): void {
     this.currentPage = page;
-    this.getMovementOfCashesV2();
+    this.getMovementOfCashes();
   }
 
-  public getMovementOfCashes(): void {
-
-    this.loading = true;
-
-    // ORDENAMOS LA CONSULTA
-    let sortAux = { expirationDate: 1 };
-    
-    // FILTRAMOS LA CONSULTA
-    let match = `{`;
-    for(let i = 0; i < this.displayedColumns2.length; i++) {
-      let value = this.filters[this.displayedColumns2[i]];
-      if (value && value != "") {
-        match += `"${this.displayedColumns2[i]}": { "$regex": "${value}", "$options": "i"},`;
-      }
-    }
-
-    match += `"operationType": { "$ne": "D" }, "transaction.type.transactionMovement" : "${this.transactionMovement}" }`;
-
-    match = JSON.parse(match);
-    
-    // CAMPOS A TRAER
-    let project = {
-      "_id" : 1,
-      "transaction.endDate": { $dateToString : { date: "$transaction.endDate", format: "%d/%m/%Y", timezone: "-03:00"}},
-      "transaction.cashBox.number" : { $toString : "$transaction.cashBox.number"},
-      "quota" :1 ,
-      "discount": 1,
-      "number" : 1,
-      "statusCheck" : 1,
-      "observation" : 1,
-      "bank._id" : 1 ,
-      "bank.name" : { $toString : '$bank.name'} , 
-      "amountPaid" :{ $toString : '$amountPaid'} ,
-      "operationType": 1,
-      "expirationDate": { $dateToString: { date: "$expirationDate", format: "%d/%m/%Y", timezone: "-03:00" }},
-      "transaction._id":1,
-      "transaction.state" : 1,
-      "transaction.type.name" : 1,
-      "transaction.type.transactionMovement" : 1,
-      "date": 1,
-      "titular" : 1,
-      "receiver" : 1,
-      "type" : 1,
-      "deliveredBy" : 1,
-      "CUIT" : 1,
-    };
-
-    // AGRUPAMOS EL RESULTADO
-    let group = {
-      _id: null,
-      count: { $sum: 1 },
-      movementOfCashes: { $push: '$$ROOT' }
-    };
-
-    let page = 0;
-    if(this.currentPage != 0) {
-      page = this.currentPage - 1;
-    }
-    let skip = !isNaN(page * this.itemsPerPage) ?
-            (page * this.itemsPerPage) :
-                0 // SKIP
-
-    this._movementOfCashService.getMovementsOfCashesV2(
-        project, // PROJECT
-        match, // MATCH
-        sortAux, // SORT
-        group, // GROUP
-        this.itemsPerPage, // LIMIT
-        skip // SKIP
-    ).subscribe(
-      result => {
-        this.loading = false;
-        if (result && result[0] && result[0].movementOfCashes) {
-          this.movementsOfCashes = result[0].movementOfCashes;
-          this.totalItems = result[0].count;
-          this.areMovementOfCashesEmpty = false;
-        } else {
-          this.movementsOfCashes = new Array();
-          this.totalItems = 0;
-          this.areMovementOfCashesEmpty = true;
-        }
-      },
-      error => {
-        this.showMessage(error._body, 'danger', false);
-        this.loading = false;
-        this.totalItems = 0;
-      }
-    );
-
-    /*this.loading = true;
-
-    this._movementOfCashService.getMovementsOfCashesByMovement(this.transactionMovement).subscribe(
-      result => {
-        if (!result.movementsOfCashes) {
-          if (result.message && result.message !== '') {
-            this.showMessage(result.message, 'info', true);
-          }
-          this.loading = false;
-          this.movementsOfCashes = new Array();
-          this.areMovementOfCashesEmpty = true;
-        } else {
-          this.hideMessage();
-          this.loading = false;
-          this.movementsOfCashes = result.movementsOfCashes;
-          console.log(this.movementsOfCashes);
-          this.totalItems = this.movementsOfCashes.length;
-          this.areMovementOfCashesEmpty = false;
-        }
-      },
-      error => {
-        this.showMessage(error._body, 'danger', false);
-        this.loading = false;
-      }
-    );
-  }
-*/
-  }
   public async selectmovementOfCash(movementOfCashSelected: MovementOfCash) {
-    if(this.transactionAmount >= movementOfCashSelected.amountPaid){
+
+    if(this.transactionAmount >= movementOfCashSelected.amountPaid) {
       let movementOfCash = await this.getMovementOfCashById(movementOfCashSelected._id);
-      this.activeModal.close(movementOfCash);
+      this.movementsOfCashesSelected.push(movementOfCash);
+      this.activeModal.close({ movementsOfCashes: this.movementsOfCashesSelected });
     } else {
       if(this.pathLocation[2] !== "fondos"){
-        this.showMessage("El cheque es mayor al monto a pagar", 'info', false);
+        this.showMessage("El monto $" + movementOfCashSelected.amountPaid + " es superior al de la transacción.", 'info', false);
       }
     }
   }
@@ -332,25 +242,21 @@ export class ListMovementOfCashesComponent implements OnInit {
     });
   }
 
-  public orderBy (term: string, property?: string): void {
+  public orderBy (term: string): void {
 
     if (this.orderTerm[0] === term) {
       this.orderTerm[0] = '-' + term;
     } else {
       this.orderTerm[0] = term;
     }
-    this.propertyTerm = property;
+
+    this.getMovementOfCashes();
   }
 
   public refresh(): void {
     this.getMovementOfCashes();
     
   }
-
-  public refreshV2() : void {
-    this.getMovementOfCashesV2();
-  }
-
 
   public openModal(op: string, movementOfCash: MovementOfCash): void {
 
