66f0f18378e88f0b30a5481ff76904874e55fdb7
unknown <androettofranco@gmail.com>
2019-08-06 09:43:07 -0300

    arreglo para generar pago/cobro desde atajo cta cte

--- src/app/components/add-transaction/add-transaction.component.ts
+++ src/app/components/add-transaction/add-transaction.component.ts
@@ -145,6 +145,9 @@ export class AddTransactionComponent implements OnInit {
     this.posType = pathLocation[2];
     this.buildForm();
 
+    console.log(this.userType);
+    console.log(this.posType)
+
     if (this.transactionId) {
       this.getTransaction();
     }
