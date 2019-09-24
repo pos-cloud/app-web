cec3f678c9d033d2ce623563bbdbc5b4ec6894a8
LLBaccillere <lbaccillere@outlook.com>
2019-09-24 18:47:16 -0300

    Se agrega config de caja por usuario.

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -28,6 +28,7 @@ import { CurrencyService } from 'app/services/currency.service';
   providers: [NgbAlertConfig, DateFormatPipe],
   encapsulation: ViewEncapsulation.None
 })
+
 export class ConfigComponent implements OnInit {
 
   public routeFile:string;
@@ -298,6 +299,9 @@ export class ConfigComponent implements OnInit {
         ]
       ],
       'company.allowCurrentAccount.default' : [ this.config.company.allowCurrentAccount.default, [
+        ]
+      ],
+      'cashBox.perUser' : [ this.config.cashBox.perUser, [
         ]
       ],
       'reports.summaryOfAccounts.detailsPaymentMethod': [this.config.reports.summaryOfAccounts.detailsPaymentMethod, [
@@ -640,11 +644,12 @@ export class ConfigComponent implements OnInit {
     }
 
     if (this.config.article.code.validators.maxLength === undefined) this.config.article.code.validators.maxLength = 10;
-    if (!this.config.company.allowCurrentAccount.default) this.config.company.allowCurrentAccount.default = false;
     if (!this.config.article.printLabel.value) this.config.article.printLabel.value = 'code';
-    if (!this.config.article.isWeigth.default) this.config.article.isWeigth.default = false;
-    if (!this.config.reports.summaryOfAccounts.invertedViewClient) this.config.reports.summaryOfAccounts.invertedViewClient = false;
-    if (!this.config.reports.summaryOfAccounts.invertedViewProvider) this.config.reports.summaryOfAccounts.invertedViewProvider = false;
+    if (this.config.article.isWeigth.default === undefined) this.config.article.isWeigth.default = false;
+    if (this.config.company.allowCurrentAccount.default === undefined) this.config.company.allowCurrentAccount.default = false;
+    if (this.config.cashBox.perUser === undefined) this.config.cashBox.perUser = false;
+    if (this.config.reports.summaryOfAccounts.invertedViewClient === undefined) this.config.reports.summaryOfAccounts.invertedViewClient = false;
+    if (this.config.reports.summaryOfAccounts.invertedViewProvider === undefined) this.config.reports.summaryOfAccounts.invertedViewProvider = false;
     if (this.config.tradeBalance.codePrefix === undefined) this.config.tradeBalance.codePrefix = 0;
     if (this.config.tradeBalance.numberOfQuantity === undefined) this.config.tradeBalance.numberOfQuantity = 2;
     if (this.config.tradeBalance.numberOfIntegers === undefined) this.config.tradeBalance.numberOfIntegers = 3;
@@ -689,10 +694,11 @@ export class ConfigComponent implements OnInit {
     this.configFormSystem.setValue({
       '_id': this.config._id,
       'article.code.validators.maxLength': this.config.article.code.validators.maxLength,
-      'company.allowCurrentAccount.default': this.config.company.allowCurrentAccount.default,
-      'company.vatCondition.default': vatConfitionDefault,
       'article.printLabel.value': this.config.article.printLabel.value,
       'article.isWeigth.default': this.config.article.isWeigth.default,
+      'company.allowCurrentAccount.default': this.config.company.allowCurrentAccount.default,
+      'company.vatCondition.default': vatConfitionDefault,
+      'cashBox.perUser': this.config.cashBox.perUser,
       'reports.summaryOfAccounts.detailsPaymentMethod': this.config.reports.summaryOfAccounts.detailsPaymentMethod,
       'reports.summaryOfAccounts.invertedViewClient': this.config.reports.summaryOfAccounts.invertedViewClient,
       'reports.summaryOfAccounts.invertedViewProvider': this.config.reports.summaryOfAccounts.invertedViewProvider,
