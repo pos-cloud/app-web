920f2c07fea8abfe6565d2f8f51446b0e834a734
LLBaccillere <lbaccillere@outlook.com>
2019-11-22 03:31:05 -0300

    Se agrega scroll con click en articulos, transaccion y resumenes de cuentas.

--- src/app/components/list-transactions/list-transactions.component.ts
+++ src/app/components/list-transactions/list-transactions.component.ts
@@ -137,6 +137,37 @@ export class ListTransactionsComponent implements OnInit {
     );
 
     this.getTransactions();
+  
+    this.initDragHorizontalScroll();
+  }
+
+  public initDragHorizontalScroll(): void {
+    const slider = document.querySelector('.table-responsive');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e['pageX'] - slider['offsetLeft'];
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e['pageX'] - slider['offsetLeft'];
+      const walk = (x - startX) * 0.7; //scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+    });
   }
 
   public getPrinters(): void {
