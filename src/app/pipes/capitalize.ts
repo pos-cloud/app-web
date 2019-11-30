abc3bc8507994baeba9f610caf52f1eb7424e917
LLBaccillere <lbaccillere@outlook.com>
2019-11-30 18:56:35 -0300

    Se corrige pipe capitalize.

--- src/app/pipes/capitalize.ts
+++ src/app/pipes/capitalize.ts
@@ -1,12 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'capitalize'
+  name: 'capitalize'
 })
+
 export class CapitalizePipe implements PipeTransform {
-	transform(value:any):any {
-		if (value) {
-			return value.charAt(0).toUpperCase() + value.slice(1);
-		}
-	}
+
+  transform(value: any): any {
+    value = value.replace('  ', ' ');
+    if (value) {
+      let w = '';
+      if (value.split(' ').length > 0) {
+        value.split(' ').forEach(word => {
+          w += word.charAt(0).toUpperCase() + word.toString().substr(1, word.length).toLowerCase() + ' '
+        });
+      } else {
+        w = value.charAt(0).toUpperCase() + value.toString().substr(1, value.length).toLowerCase();
+      }
+      return w;
+    }
+    return value;
+  }
 }
