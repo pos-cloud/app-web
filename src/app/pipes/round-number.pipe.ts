bbf68ed6d7ba09b2bbeaea7ab0da71ee60803b93
LLBaccillere <lbaccillere@outlook.com>
2020-01-02 18:50:55 -0300

    Se agrega round number para entero sin decimales.

--- src/app/pipes/round-number.pipe.ts
+++ src/app/pipes/round-number.pipe.ts
@@ -6,22 +6,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RoundNumberPipe implements PipeTransform {
 	transform(value: any, numberOfDecimals: number = 2): any {
 		if (value) {
-      if (!isNaN(value)) {
-        switch (numberOfDecimals) {
-          case 1:
-            return Math.round(value * 10) / 10;
-          case 2:
-            return Math.round(value * 100) / 100;
-          case 3:
-            return Math.round(value * 1000) / 1000;
-          case 4:
-            return Math.round(value * 1000) / 1000;
-          default:
-            return Math.round(value * 100) / 100;
-        }
-      } else {
-        return parseFloat(value.toFixed(numberOfDecimals));
-      }
+			if (!isNaN(value)) {
+				switch (numberOfDecimals) {
+					case 0:
+						return Math.round(value * 1) / 1;
+					case 1:
+						return Math.round(value * 10) / 10;
+					case 2:
+						return Math.round(value * 100) / 100;
+					case 3:
+						return Math.round(value * 1000) / 1000;
+					case 4:
+						return Math.round(value * 1000) / 1000;
+					default:
+						return Math.round(value * 100) / 100;
+				}
+			} else {
+				return parseFloat(value.toFixed(numberOfDecimals));
+			}
 		} else {
 			if (value === 0) {
 				return 0;
