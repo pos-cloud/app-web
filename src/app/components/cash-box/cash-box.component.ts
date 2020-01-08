a4eac43e1000450cec3dfd4e2f4eb3cf69b651ac
LLBaccillere <lbaccillere@outlook.com>
2020-01-08 20:26:06 -0300

    Corrección de definicio de array.

--- src/app/components/cash-box/cash-box.component.ts
+++ src/app/components/cash-box/cash-box.component.ts
@@ -38,9 +38,9 @@ export class CashBoxComponent implements OnInit {
 
 	public cashBoxForm: FormGroup;
 	public paymentMethods: PaymentMethod[];
-	public currencyValues : CurrencyValue[];
+	public currencyValues: CurrencyValue[];
 	public transaction: Transaction;
-	public currencyValuesForm : [{value : number, quantity : number }] = [{ value : null , quantity : null}]
+	public currencyValuesForm: { value: number, quantity: number }[];
 	public totalCurrencyValue = 0;
 	public cashBox: CashBox;
 	public movementOfCash: MovementOfCash;
@@ -61,7 +61,7 @@ export class CashBoxComponent implements OnInit {
 		private _cashBoxService: CashBoxService,
 		private _authService: AuthService,
 		private _transactionService: TransactionService,
-		private _currencyValueService : CurrencyValueService,
+		private _currencyValueService: CurrencyValueService,
 		private _configService: ConfigService,
 		public activeModal: NgbActiveModal,
 		public alertConfig: NgbAlertConfig,
@@ -210,8 +210,8 @@ export class CashBoxComponent implements OnInit {
 		let values = {
 			'paymentMethod': paymentMethod,
 			'amount': 0,
-			'currencyValue' : null,
-			'currencyAmount' : 0
+			'currencyValue': null,
+			'currencyAmount': 0
 		};
 
 		this.cashBoxForm.setValue(values);
@@ -420,36 +420,36 @@ export class CashBoxComponent implements OnInit {
 			{ "operationType": { "$ne": "D" } },
 			{},
 			{},
-			).subscribe(
-				result => {
-					if (result && result.currencyValues) {
-						this.currencyValues = result.currencyValues
-						console.log(this.currencyValues)
-					}
-				},
-				error => {
-					this.showMessage(error._body, 'danger', false);
+		).subscribe(
+			result => {
+				if (result && result.currencyValues) {
+					this.currencyValues = result.currencyValues
+					console.log(this.currencyValues)
 				}
-			)
-		
+			},
+			error => {
+				this.showMessage(error._body, 'danger', false);
+			}
+		)
+
 	}
 
-	public addCurrencyValue(e): void{
-		this.currencyValuesForm.push({ value : parseInt(e.currencyValue) , quantity : e.currencyAmount})
+	public addCurrencyValue(e): void {
+		if(!this.currencyValuesForm) this.currencyValuesForm = new Array();
+		this.currencyValuesForm.push({ value: parseInt(e.currencyValue), quantity: e.currencyAmount })
 		this.totalCurrencyValue = 0;
 		for (const iterator of this.currencyValuesForm) {
-			this.totalCurrencyValue = this.totalCurrencyValue + ( iterator.quantity * iterator.value)
-		}	
-	}
-
-	public deleteCurrencyValue(e) : void {
-		this.currencyValuesForm.splice( e, 1 );
-		this.totalCurrencyValue = 0;
-		for (const iterator of this.currencyValuesForm) {
-			this.totalCurrencyValue = this.totalCurrencyValue + ( iterator.quantity * iterator.value)
+			this.totalCurrencyValue = this.totalCurrencyValue + (iterator.quantity * iterator.value)
 		}
 	}
-	
+
+	public deleteCurrencyValue(e): void {
+		this.totalCurrencyValue = 0;
+		for (const iterator of this.currencyValuesForm.splice(e, 1)) {
+			this.totalCurrencyValue = this.totalCurrencyValue + (iterator.quantity * iterator.value)
+		}
+	}
+
 	public addMovementOfCash(): void {
 
 		this.movementOfCash.type = this.cashBoxForm.value.paymentMethod;
@@ -462,11 +462,11 @@ export class CashBoxComponent implements OnInit {
 			mov.discount = this.movementOfCash.discount;
 			mov.surcharge = this.movementOfCash.surcharge;
 
-			if(this.movementOfCash.type.allowCurrencyValue && this.currencyValuesForm && this.currencyValuesForm.length >0){
+			if (this.movementOfCash.type.allowCurrencyValue && this.currencyValuesForm && this.currencyValuesForm.length > 0) {
 				mov.currencyValue = this.currencyValuesForm
 				mov.amountPaid = 0;
 				mov.currencyValue.forEach(element => {
-					mov.amountPaid = mov.amountPaid + (element.quantity*element.value)
+					mov.amountPaid = mov.amountPaid + (element.quantity * element.value)
 				});
 			} else {
 				mov.amountPaid = this.movementOfCash.amountPaid;
