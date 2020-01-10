ad48f12640aa496dc46c3da3101616c61689b6d7
unknown <androettofranco@gmail.com>
2020-01-09 23:51:29 -0300

    agrega scroll horizaontal a list companies pone titulo oculta botones cuando es view company saca paginator de select company

--- src/app/components/list-companies/list-companies.component.ts
+++ src/app/components/list-companies/list-companies.component.ts
@@ -97,7 +97,37 @@ export class ListCompaniesComponent implements OnInit {
 			this.userType = pathLocation[1];
 		}
 		this.getItems();
+		this.initDragHorizontalScroll();
 	}
+
+	public initDragHorizontalScroll(): void {
+		const slider = document.querySelector('.table-responsive');
+		let isDown = false;
+		let startX;
+		let scrollLeft;
+	
+		slider.addEventListener('mousedown', (e) => {
+		  isDown = true;
+		  slider.classList.add('active');
+		  startX = e['pageX'] - slider['offsetLeft'];
+		  scrollLeft = slider.scrollLeft;
+		});
+		slider.addEventListener('mouseleave', () => {
+		  isDown = false;
+		  slider.classList.remove('active');
+		});
+		slider.addEventListener('mouseup', () => {
+		  isDown = false;
+		  slider.classList.remove('active');
+		});
+		slider.addEventListener('mousemove', (e) => {
+		  if(!isDown) return;
+		  e.preventDefault();
+		  const x = e['pageX'] - slider['offsetLeft'];
+		  const walk = (x - startX) * 0.7; //scroll-fast
+		  slider.scrollLeft = scrollLeft - walk;
+		});
+	  }
 
 	ngAfterViewInit() {
 		this.focusEvent.emit(true);
