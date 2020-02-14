d478f47c0e0581bb2eb9a35baa19ef8472e0d77e
unknown <androettofranco@gmail.com>
2020-02-14 09:38:12 -0300

    arregla eliminar

--- src/app/components/list-transactions/list-transactions.component.ts
+++ src/app/components/list-transactions/list-transactions.component.ts
@@ -237,18 +237,8 @@ export class ListTransactionsComponent implements OnInit {
 				}
 				j++;
 
-				if(!this.columns[i].project) {
-					if (this.columns[i].datatype !== "string") {
-						if (this.columns[i].datatype === "date") {
-                            project += `"${this.columns[i].name}": 1`;
-
-							//project += `"${this.columns[i].name}": { "$dateToString": { "date": "$${this.columns[i].name}", "format": "%d/%m/%Y", "timezone": "${this.timezone}" }}`
-						} else {
-							project += `"${this.columns[i].name}": { "$toString" : "$${this.columns[i].name}" }`
-						}
-					} else {
-						project += `"${this.columns[i].name}": 1`;
-					}
+				if(this.columns[i].project === null) {
+                    project += `"${this.columns[i].name}": 1`;
 				} else {
 					project += `"${this.columns[i].name}": ${this.columns[i].project}`;
 				}
