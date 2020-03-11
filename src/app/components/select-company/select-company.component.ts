7b6ed1f152cd1e8164aa8c9bf93791bdb7cdb8a9
unknown <androettofranco@gmail.com>
2020-03-11 16:21:23 -0300

    pasa a v2 select company ;)

--- src/app/components/select-company/select-company.component.ts
+++ src/app/components/select-company/select-company.component.ts
@@ -37,16 +37,23 @@ export class SelectCompanyComponent implements OnInit {
 	public userCountry: string;
 	private subscription: Subscription = new Subscription();
 	public focusEvent = new EventEmitter<boolean>();
-	public filterName;
-	public filterFantasyName;
-	public filterIdentificationValue;
-	public filterAddress;
-	public filterAddressNumber;
-	public filterPhones;
-	public filterCode;
-	public p;
-	public filterVATCondition;
+    public currentPage: number = 0;
+    public displayedColumns = [
+        "_id",
+        "name" ,
+        "fantasyName", 
+        "identificationValue" ,
+        "address" ,
+        "addressNumber" ,
+        "phones" ,
+        "vatCondition.description" ,
+        "type", 
+        "operationType"
+      ];
 
+    public filters: any[];
+    public filterValue: string;
+  
 	constructor(
 		public _companyService: CompanyService,
 		public _router: Router,
@@ -55,7 +62,11 @@ export class SelectCompanyComponent implements OnInit {
 		public alertConfig: NgbAlertConfig,
 		public _authService: AuthService
 	) {
-		this.companies = new Array();
+        this.companies = new Array();
+        this.filters = new Array();
+        for(let field of this.displayedColumns) {
+            this.filters[field] = "";
+        }
 	}
 
 	ngOnInit(): void {
@@ -83,30 +94,72 @@ export class SelectCompanyComponent implements OnInit {
 
 		let pathLocation: string[] = this._router.url.split('/');
 
-		if (!this.type) {
-			if (pathLocation[2] === "clientes") {
-				this.type = CompanyType.Client;
-			} else if (pathLocation[2] === "proveedores") {
-				this.type = CompanyType.Provider;
-			}
-		}
+		this.loading = true;
+  
+      // ORDENAMOS LA CONSULTA
+      let sortAux;
+      if (this.orderTerm[0].charAt(0) === '-') {
+          sortAux = `{ "${this.orderTerm[0].split('-')[1]}" : -1 }`;
+      } else {
+          sortAux = `{ "${this.orderTerm[0]}" : 1 }`;
+      }
+      sortAux = JSON.parse(sortAux);
+      
+      // FILTRAMOS LA CONSULTA
+      let match = `{`;
+      for(let i = 0; i < this.displayedColumns.length; i++) {
+        let value = this.filters[this.displayedColumns[i]];
+        if (value && value != "") {
+          match += `"${this.displayedColumns[i]}": { "$regex": "${value}", "$options": "i"},`;
+        }
+      }
+  
+      match += `"operationType": { "$ne": "D" },
+                "type" : "${this.type.toString()}"}`;
+      
+      match = JSON.parse(match);
 
-		let query = 'where="type":"' + this.type.toString() + '"';
+        let project = {
+            _id : 1,
+            name : 1,
+            fantasyName : 1,
+            identificationValue : 1,
+            address : 1,
+            addressNumber : 1,
+            phones : 1,
+            "vatCondition.description" : 1,
+            type : 1,
+            operationType : 1
+        }
 
-		this.subscription.add(this._companyService.getCompanies(query).subscribe(
+        let group = {
+            _id: null,
+            count: { $sum: 1 },
+            companies: { $push: '$$ROOT' }
+          };
+
+          let page = 0;
+        if (this.currentPage != 0) {
+            page = this.currentPage - 1;
+        }
+        let skip = !isNaN(page * this.itemsPerPage) ?
+            (page * this.itemsPerPage) :
+            0 // SKIP
+        let limit = this.itemsPerPage;
+
+		this.subscription.add(this._companyService.getCompaniesV2(project,match,{ name : 1},group,limit,skip).subscribe(
 			result => {
-				if (!result.companies) {
-					if (result.message && result.message !== '') this.showMessage(result.message, 'info', true);
+                if (result && result[0] && result[0].companies) {
+                    this.hideMessage();
 					this.loading = false;
-					this.companies = new Array();
-					this.areCompaniesEmpty = true;
-				} else {
-					this.hideMessage();
-					this.loading = false;
-					this.companies = result.companies;
+					this.companies = result[0].companies;
 					this.totalItems = this.companies.length;
 					this.areCompaniesEmpty = false;
-				}
+                  } else {
+                    this.loading = false;
+					this.companies = new Array();
+					this.areCompaniesEmpty = true;
+                  }
 			},
 			error => {
 				this.showMessage(error._body, 'danger', false);
@@ -115,15 +168,21 @@ export class SelectCompanyComponent implements OnInit {
 		));
 	}
 
-	public orderBy(term: string, property?: string): void {
+    public pageChange(page): void {
+        this.currentPage = page;
+        this.getCompaniesByType();
+    }
 
-		if (this.orderTerm[0] === term) {
-			this.orderTerm[0] = "-" + term;
-		} else {
-			this.orderTerm[0] = term;
-		}
-		this.propertyTerm = property;
-	}
+    public orderBy (term: string): void {
+  
+        if (this.orderTerm[0] === term) {
+          this.orderTerm[0] = '-' + term;
+        } else {
+          this.orderTerm[0] = term;
+        }
+    
+        this.getCompaniesByType();
+      }
 
 	public refresh(): void {
 		this.getCompaniesByType();
