6d4a542cae2f16f29acb65fb761965966e7d95bd
Lucas Baccillere <lbaccillereoo@outlook.com>
2020-07-07 02:39:11 -0300

    Se agrega corrección de stock.

--- src/app/components/article-stock/article-stock.service.ts
+++ src/app/components/article-stock/article-stock.service.ts
@@ -13,205 +13,227 @@ import { MovementOfArticle } from 'app/components/movement-of-article/movement-o
 @Injectable()
 export class ArticleStockService {
 
-    constructor(
-        private _http: HttpClient,
-        private _authService: AuthService
-    ) { }
+  constructor(
+    private _http: HttpClient,
+    private _authService: AuthService
+  ) { }
 
-    public getArticleStock(_id: string): Observable<any> {
+  public getArticleStock(_id: string): Observable<any> {
 
-        const URL = `${Config.apiURL}article-stock`;
+    const URL = `${Config.apiURL}article-stock`;
 
-        const headers = new HttpHeaders()
-            .set('Content-Type', 'application/json')
-            .set('Authorization', this._authService.getToken());
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this._authService.getToken());
 
-        const params = new HttpParams()
-            .set('id', _id);
+    const params = new HttpParams()
+      .set('id', _id);
 
-        return this._http.get(URL, {
-            headers: headers,
-            params: params
-        }).pipe(
-            map(res => {
-                return res;
-            }),
-            catchError((err) => {
-                return of(err);
-            })
-        );
-    }
+    return this._http.get(URL, {
+      headers: headers,
+      params: params
+    }).pipe(
+      map(res => {
+        return res;
+      }),
+      catchError((err) => {
+        return of(err);
+      })
+    );
+  }
 
-    public getArticleStocks(
-        query?: string
-    ): Observable<any> {
+  public getArticleStocks(
+    query?: string
+  ): Observable<any> {
 
-        const URL = `${Config.apiURL}article-stocks`;
+    const URL = `${Config.apiURL}article-stocks`;
 
-        const headers = new HttpHeaders()
-            .set('Content-Type', 'application/json')
-            .set('Authorization', this._authService.getToken());
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this._authService.getToken());
 
-        const params = new HttpParams()
-            .set('query', query);
+    const params = new HttpParams()
+      .set('query', query);
 
-        return this._http.get(URL, {
-            headers: headers,
-            params: params
-        }).pipe(
-            map(res => {
-                return res;
-            }),
-            catchError((err) => {
-                return of(err);
-            })
-        );
-    }
+    return this._http.get(URL, {
+      headers: headers,
+      params: params
+    }).pipe(
+      map(res => {
+        return res;
+      }),
+      catchError((err) => {
+        return of(err);
+      })
+    );
+  }
 
-    public getArticleStocksV2(
-        project: {},
-        match: {},
-        sort: {},
-        group: {},
-        limit: number = 0,
-        skip: number = 0
-    ): Observable<any> {
+  public getArticleStocksV2(
+    project: {},
+    match: {},
+    sort: {},
+    group: {},
+    limit: number = 0,
+    skip: number = 0
+  ): Observable<any> {
 
-        const URL = `${Config.apiURL}v2/article-stocks`;
+    const URL = `${Config.apiURL}v2/article-stocks`;
 
-        const headers = new HttpHeaders()
-            .set('Content-Type', 'application/json')
-            .set('Authorization', this._authService.getToken());
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this._authService.getToken());
 
-        const params = new HttpParams()
-            .set('project', JSON.stringify(project))
-            .set('match', JSON.stringify(match))
-            .set('sort', JSON.stringify(sort))
-            .set('group', JSON.stringify(group))
-            .set('limit', limit.toString())
-            .set('skip', skip.toString());
+    const params = new HttpParams()
+      .set('project', JSON.stringify(project))
+      .set('match', JSON.stringify(match))
+      .set('sort', JSON.stringify(sort))
+      .set('group', JSON.stringify(group))
+      .set('limit', limit.toString())
+      .set('skip', skip.toString());
 
-        return this._http.get(URL, {
-            headers: headers,
-            params: params
-        }).pipe(
-            map(res => {
-                return res;
-            }),
-            catchError((err) => {
-                return of(err);
-            })
-        );
-    }
+    return this._http.get(URL, {
+      headers: headers,
+      params: params
+    }).pipe(
+      map(res => {
+        return res;
+      }),
+      catchError((err) => {
+        return of(err);
+      })
+    );
+  }
 
-    public saveArticleStock(articleStock: ArticleStock): Observable<any> {
+  public saveArticleStock(articleStock: ArticleStock): Observable<any> {
 
-        const URL = `${Config.apiURL}article-stock`;
+    const URL = `${Config.apiURL}article-stock`;
 
-        const headers = new HttpHeaders()
-            .set('Content-Type', 'application/json')
-            .set('Authorization', this._authService.getToken());
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this._authService.getToken());
 
-        return this._http.post(URL, articleStock, {
-            headers: headers
-        }).pipe(
-            map(res => {
-                return res;
-            }),
-            catchError((err) => {
-                return of(err);
-            })
-        );
-    }
+    return this._http.post(URL, articleStock, {
+      headers: headers
+    }).pipe(
+      map(res => {
+        return res;
+      }),
+      catchError((err) => {
+        return of(err);
+      })
+    );
+  }
 
-    public updateArticleStock(articleStock: ArticleStock): Observable<any> {
+  public updateArticleStock(articleStock: ArticleStock): Observable<any> {
 
-        const URL = `${Config.apiURL}article-stock`;
+    const URL = `${Config.apiURL}article-stock`;
 
-        const headers = new HttpHeaders()
-            .set('Content-Type', 'application/json')
-            .set('Authorization', this._authService.getToken());
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this._authService.getToken());
 
-        const params = new HttpParams()
-            .set('id', articleStock._id);
+    const params = new HttpParams()
+      .set('id', articleStock._id);
 
-        return this._http.put(URL, articleStock, {
-            headers: headers,
-            params: params
-        }).pipe(
-            map(res => {
-                return res;
-            }),
-            catchError((err) => {
-                return of(err);
-            })
-        );
-    }
+    return this._http.put(URL, articleStock, {
+      headers: headers,
+      params: params
+    }).pipe(
+      map(res => {
+        return res;
+      }),
+      catchError((err) => {
+        return of(err);
+      })
+    );
+  }
 
-    public updateRealStock(movementOfArticle: MovementOfArticle): Observable<any> {
+  public updateRealStock(movementOfArticle: MovementOfArticle): Observable<any> {
 
-        const URL = `${Config.apiURL}amount-stock-by-article`;
+    const URL = `${Config.apiURL}amount-stock-by-article`;
 
-        const headers = new HttpHeaders()
-            .set('Content-Type', 'application/json')
-            .set('Authorization', this._authService.getToken());
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this._authService.getToken());
 
-        return this._http.put(URL, {
-            movementOfArticle: movementOfArticle
-        }, {
-            headers: headers
-        }).pipe(
-            map(res => {
-                return res;
-            }),
-            catchError((err) => {
-                return of(err);
-            })
-        );
-    }
+    return this._http.put(URL, {
+      movementOfArticle: movementOfArticle
+    }, {
+      headers: headers
+    }).pipe(
+      map(res => {
+        return res;
+      }),
+      catchError((err) => {
+        return of(err);
+      })
+    );
+  }
 
-    public deleteArticleStock(_id: string): Observable<any> {
+  public updateObjByArticle(movementOfArticle: MovementOfArticle): Observable<any> {
 
-        const URL = `${Config.apiURL}article-stock`;
+    const URL = `${Config.apiV8URL}stock/by-article`;
 
-        const headers = new HttpHeaders()
-            .set('Content-Type', 'application/json')
-            .set('Authorization', this._authService.getToken());
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this._authService.getToken());
 
-        const params = new HttpParams()
-            .set('id', _id);
+    return this._http.put(URL, {
+      movementOfArticle: movementOfArticle
+    }, {
+      headers: headers
+    }).pipe(
+      map(res => {
+        return res;
+      }),
+      catchError((err) => {
+        return of(err);
+      })
+    );
+  }
 
-        return this._http.delete(URL, {
-            headers: headers,
-            params: params
-        }).pipe(
-            map(res => {
-                return res;
-            }),
-            catchError((err) => {
-                return of(err);
-            })
-        );
-    }
+  public deleteArticleStock(_id: string): Observable<any> {
 
-    public updateArticle()
-        : Observable<any> {
+    const URL = `${Config.apiURL}article-stock`;
 
-        const URL = `${Config.apiURL}distribuidoragiletta/update-article`;
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this._authService.getToken());
 
-        const headers = new HttpHeaders()
-            .set('Content-Type', 'application/json')
-            .set('Authorization', this._authService.getToken());
+    const params = new HttpParams()
+      .set('id', _id);
 
-        return this._http.post(URL, {
-            headers: headers
-        }).pipe(
-            map(res => {
-                return res;
-            }),
-            catchError((err) => {
-                return of(err);
-            })
-        );
-    }
+    return this._http.delete(URL, {
+      headers: headers,
+      params: params
+    }).pipe(
+      map(res => {
+        return res;
+      }),
+      catchError((err) => {
+        return of(err);
+      })
+    );
+  }
+
+  public updateArticle()
+    : Observable<any> {
+
+    const URL = `${Config.apiURL}distribuidoragiletta/update-article`;
+
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this._authService.getToken());
+
+    return this._http.post(URL, {
+      headers: headers
+    }).pipe(
+      map(res => {
+        return res;
+      }),
+      catchError((err) => {
+        return of(err);
+      })
+    );
+  }
 }
