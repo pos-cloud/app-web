bbebc07d1cc26c1f67691af1b658c0d8456afa15
Franco <androettofranco@gmail.com>
2021-02-25 17:40:36 -0300

    agrega servicio para creacion de asiento

--- src/app/components/account-seat/account-seat.service.ts
+++ src/app/components/account-seat/account-seat.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+
 import { ModelService } from '../model/model.service';
 import { AuthService } from '../login/auth.service';
+import { Config } from "app/app.config";
+
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
+import { of } from "rxjs";
+import { Observable } from "rxjs/Observable";
+import { map, catchError } from "rxjs/operators";
 
 @Injectable()
 export class AccountSeatService extends ModelService {
@@ -16,4 +22,25 @@ export class AccountSeatService extends ModelService {
       _authService
     );
   }
+
+  public addAccountSeatByTransaction(transactionId: string): Observable<any> {
+
+    const URL = `${Config.apiV8URL}account-seat-transaction`;
+
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this._authService.getToken());
+
+    return this._http.post(URL+`/${transactionId}`,{}, {
+      headers: headers
+    }).pipe(
+      map(res => {
+        return res;
+      }),
+      catchError((err) => {
+        return of(err);
+      })
+    );
+  }
+
 }
