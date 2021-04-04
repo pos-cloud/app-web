926c71d4e8eb7c6d4b868ab3ca62b082898776d1
Franco <androettofranco@gmail.com>
2021-04-04 20:31:31 -0300

    agrega suma a movimiento de medios

--- src/app/components/movement-of-cash/list-movements-of-cashes/list-movements-of-cashes.component.ts
+++ src/app/components/movement-of-cash/list-movements-of-cashes/list-movements-of-cashes.component.ts
@@ -306,6 +306,7 @@ export class ListMovementOfCashesComponent implements OnInit {
                     } else {
                         this.items = result[0].items;
                         this.totalItems = result[0].count;
+                        this.getSum();
                     }
                 } else {
                     this.items = new Array();
@@ -318,6 +319,23 @@ export class ListMovementOfCashesComponent implements OnInit {
                 this.totalItems = 0;
             }
         ));
+    }
+
+    public getSum(): any {
+        var total = 0
+        this.columns.forEach(elementC => {
+            if (elementC.datatype === 'number' || elementC.datatype === 'currency') {
+                this.items.forEach(elementI => {
+                    Object.keys(elementI).forEach(elementK => {
+                        if (elementK === elementC.name) {
+                            total = total + elementI[elementK];
+                        }
+                    });
+                });
+            }
+            elementC['sum'] = total;
+            total = 0;
+        });
     }
 
     public exportItems(): void {
