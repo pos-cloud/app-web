01bb6ef15ee83679a57d6ede0e5b3be7ef429815
lucas <lbaccillere@outlook.com>
2021-06-04 15:58:15 -0300

    Ponemos para recargar los articulos cuando se carga uno nuevo o se actualiza.

--- src/app/components/article/article/add-article.component.ts
+++ src/app/components/article/article/add-article.component.ts
@@ -1565,6 +1565,7 @@ export class AddArticleComponent implements OnInit {
           } else {
             this.hasChanged = true;
             this.article = result.article;
+            this._articleService.setItems(null);
             if (this.filesToUpload) {
               this._articleService.makeFileRequest(this.article._id, this.filesToUpload)
                 .then(
@@ -1620,6 +1621,7 @@ export class AddArticleComponent implements OnInit {
           } else {
             this.hasChanged = true;
             this.article = result.article;
+            this._articleService.setItems(null);
             if (this.filesToUpload) {
               this._articleService.makeFileRequest(this.article._id, this.filesToUpload)
                 .then(
