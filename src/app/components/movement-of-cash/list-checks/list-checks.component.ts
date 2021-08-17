0941e72341cc8d99e72c927a01d3bf1c17736652
Franco <androettofranco@gmail.com>
2021-08-17 09:08:59 -0300

    agrega tipo en column de cheques

--- src/app/components/movement-of-cash/list-checks/list-checks.component.ts
+++ src/app/components/movement-of-cash/list-checks/list-checks.component.ts
@@ -162,6 +162,16 @@ export class ListChecksComponent implements OnInit {
             required: false,
         },
         {
+            name: 'type.name',
+            visible: false,
+            disabled: false,
+            filter: true,
+            datatype: 'string',
+            project: null,
+            align: 'left',
+            required: false,
+        },
+        {
             name: 'operationType',
             visible: false,
             disabled: true,
