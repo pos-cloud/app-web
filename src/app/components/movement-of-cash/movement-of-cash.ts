8a0ad130a213e10227d2029c4184a43cf36e64f4
Franco <androettofranco@gmail.com>
2021-08-18 11:01:04 -0300

    agrega identificacion de empresa

--- src/app/components/movement-of-cash/movement-of-cash.ts
+++ src/app/components/movement-of-cash/movement-of-cash.ts
@@ -83,6 +83,16 @@ export let attributes = [
         required: false,
     },
     {
+        name: 'transaction.company.identificationValue',
+        visible: false,
+        disabled: false,
+        filter: true,
+        datatype: 'string',
+        project: null,
+        align: 'left',
+        required: false,
+    },
+    {
         name: 'type._id',
         visible: false,
         disabled: true,
