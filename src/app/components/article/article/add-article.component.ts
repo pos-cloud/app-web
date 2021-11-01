4a22039164e6e07c11fec109948455ff8002a6b3
agustinDiazN <agustindiazn22@gmail.com>
2021-11-01 05:33:38 -0300

    //  providers

--- src/app/components/article/article/add-article.component.ts
+++ src/app/components/article/article/add-article.component.ts
@@ -1338,15 +1338,15 @@ export class AddArticleComponent implements OnInit {
     }
 
     let providers;
-    if (!this.article.providers || this.article.providers.length === 0) {
-      providers = null;
-    } else {
-      if (this.article.providers[0]._id) {
-        providers = this.article.providers[0]._id;
-      } else {
-        providers = this.article.providers;
-      }
-    }
+    // if (!this.article.providers || this.article.providers.length === 0) {
+    //   providers = null;
+    // } else {
+    //   if (this.article.providers[0]._id) {
+    //     providers = this.article.providers[0]._id;
+    //   } else {
+    //     providers = this.article.providers;
+    //   }
+    // }
 
     let provider;
     if (!this.article.provider || this.article.provider === null) {
