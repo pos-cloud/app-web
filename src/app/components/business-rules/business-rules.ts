5a937b0371097e70869393a88050255a239efe27
Lucas Baccillere <lbaccillere@outlook.com>
2022-05-13 11:40:31 -0300

    quitar public

--- src/app/components/business-rules/business-rules.ts
+++ src/app/components/business-rules/business-rules.ts
@@ -4,65 +4,64 @@ import { Article } from '../article/article';
 
 export class BusinessRule extends Model {
 
-    public _id: string;
-    public name: string;
-	public startDate: string;
-	public endDate: string;
-	public quantity : number;
+  _id: string;
+  name: string;
+  startDate: string;
+  endDate: string;
+  quantity: number;
+  discountAmount: number;
+  discountPercent: number;
+  article: Article;
+  newUser: boolean = false;
 
-	public discountAmount: number;
-	public discountPercent: number;
-	public article : Article;
-	public newUser : boolean = false;
+  constructor() { super(); }
 
-    constructor() { super(); }
-
-    static getAttributes(): IAttribute[] {
-        return Model.getAttributes([
-            {
-                name: 'name',
-                visible: true,
-                filter: true,
-                datatype: 'string',
-                align: 'left',
-            }, {
-                name: 'startDate',
-                visible: true,
-                filter: true,
-                project: `{ "$dateToString": { "date": "$startDate", "format": "%d/%m/%Y", "timezone": "-03:00" } }`,
-                datatype: 'string',
-                align: 'left',
-            },
-            {
-                name: 'endDate',
-                visible: true,
-                filter: true,
-                project: `{ "$dateToString": { "date": "$endDate", "format": "%d/%m/%Y", "timezone": "-03:00" } }`,
-                datatype: 'string',
-                align: 'left',
-            },
-            {
-                name: 'quantity',
-                visible: true,
-                filter: true,
-                datatype: 'string',
-                align: 'left',
-            }
-        ])
-    }
+  static getAttributes(): IAttribute[] {
+    return Model.getAttributes([
+      {
+        name: 'name',
+        visible: true,
+        filter: true,
+        datatype: 'string',
+        align: 'left',
+      }, {
+        name: 'startDate',
+        visible: true,
+        filter: true,
+        project: `{ "$dateToString": { "date": "$startDate", "format": "%d/%m/%Y", "timezone": "-03:00" } }`,
+        datatype: 'string',
+        align: 'left',
+      },
+      {
+        name: 'endDate',
+        visible: true,
+        filter: true,
+        project: `{ "$dateToString": { "date": "$endDate", "format": "%d/%m/%Y", "timezone": "-03:00" } }`,
+        datatype: 'string',
+        align: 'left',
+      },
+      {
+        name: 'quantity',
+        visible: true,
+        filter: true,
+        datatype: 'string',
+        align: 'left',
+      }
+    ])
+  }
 }
 
 export enum Types {
-    Asset = <any>"Activo",
-    Passive = <any>"Pasivo",
-    netWorth = <any>"Patrimonio Neto",
-    Result = <any>"Resultado",
-    Compensatory = <any>"Compensatoria",
-    Other = <any>"Otro"
+  Asset = <any>"Activo",
+  Passive = <any>"Pasivo",
+  netWorth = <any>"Patrimonio Neto",
+  Result = <any>"Resultado",
+  Compensatory = <any>"Compensatoria",
+  Other = <any>"Otro"
 }
 
 export enum Modes {
-    Synthetic = <any>"Sintetico",
-    Analytical = <any>"Analitico"
+  Synthetic = <any>"Sintetico",
+  Analytical = <any>"Analitico"
 }
 
