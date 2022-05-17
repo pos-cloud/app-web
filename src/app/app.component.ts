46f2d7c8504f87251d852274b0151e2b6fd5b42a
Franco <androettofranco@gmail.com>
2022-05-17 00:12:48 -0300

    set https

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -206,11 +206,11 @@ export class AppComponent {
             }
         } else {
             if(subdominio === "testing"){
-                Config.setApiHost('http://181.13.244.217:300'); // TEST
-                Config.setApiV8Host('http://181.13.244.217:308'); // TEST
-            } else {
                 Config.setApiHost('https://tapi.poscloud.com.ar'); // TEST
-                Config.setApiV8Host('https://tapiv2.poscloud.com.ar'); // TEST
+                Config.setApiV8Host('https://tapi.poscloud.com.ar'); // TEST
+            } else {
+                Config.setApiHost('http://localhost:300'); // TEST
+                Config.setApiV8Host('http://localhost:308'); // TEST
             }
         }
         console.log(environment.production);
