9232361db3489643f94f018aaafc2f8e2eab2bc4
Franco Androetto <androettofranco@gmail.com>
2023-01-17 18:43:01 -0300

    prueba test

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    api: "https://tapi.poscloud.com.ar/api",
+    api: "https://tapi.poscloud.com.ar",
     apiv2: "https://tapiv2.poscloud.com.ar"
 };  
