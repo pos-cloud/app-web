bbd3f78a75d4f19793a9a84ee4c934b74ec3da08
Franco Androetto <androettofranco@gmail.com>
2023-06-14 17:42:25 -0300

    style: change api dev

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,5 +1,12 @@
+// export const environment = {
+//     production: false,
+//     api: "https://tapi.poscloud.com.ar",
+//     apiv2: "https://tapiv2.poscloud.com.ar"
+// };  
+
+
 export const environment = {
     production: false,
-    api: "https://tapi.poscloud.com.ar",
-    apiv2: "https://tapiv2.poscloud.com.ar"
+    api: "http://vps-2504224-x.dattaweb.com:300",
+    apiv2: "http://vps-2504224-x.dattaweb.com:308"
 };  
