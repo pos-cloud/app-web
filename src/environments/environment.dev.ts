5392f3a4c0753522c0248b5567a4c2819b48a945
Franco Androetto <androettofranco@gmail.com>
2023-08-27 17:31:19 -0300

    build: update dev

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    api: "https://dev-apiv1.eu-4.evennode.com",
+    apiv2: "https://dev-apiv2.eu-4.evennode.com"
+};
--- src/environments/environment.evennode.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    api: "https://apiv1.eu-4.evennode.com",
-    apiv2: "https://apiv2.eu-4.evennode.com"
-};
