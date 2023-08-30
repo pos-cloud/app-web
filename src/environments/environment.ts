d8fc515150830e960170b3aea2610031f96dbc22
jeremias vallejos <jeremias.vallejos78gmail.com>
2023-08-30 09:18:34 -0300

    feat: change environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  api: "http://localhost:3000",
-  apiv2: "http://localhost:308",
+  api: "https://dev-apiv1.eu-4.evennode.com",
+  apiv2: "https://dev-apiv2.eu-4.evennode.com",
   apiPrint:'http://localhost:3001'
 };
