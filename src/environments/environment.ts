8849c7e482fee5622315c3aab3f294f9fc446a88
Franco Androetto <androettofranco@gmail.com>
2023-09-19 20:23:28 -0300

    build: add endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  api: "https://dev-apiv1.eu-4.evennode.com",
-  apiv2: "https://dev-apiv2.eu-4.evennode.com",
+  api: "http://localhost:300",
+  apiv2: "http://localhost:308",
   apiPrint:'http://localhost:300'
 };
