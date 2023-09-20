538c40659f988bf21281cd9ed697e0fe47eb0fd4
Franco Androetto <androettofranco@gmail.com>
2023-09-19 21:59:29 -0300

    build: delete coment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,4 @@ export const environment = {
   api: "https://api.poscloud.com.ar",
   apiv2: "https://apiv8.poscloud.com.ar",
   apiPrint: "https://api-pdf.eu-4.evennode.com"
-  // api: "http://vps-1883265-x.dattaweb.com:300",
-  // apiv2: "http://vps-1883265-x.dattaweb.com:308"
 };
