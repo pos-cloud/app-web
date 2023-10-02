507a28f08c122bba3cb4b73ee27a2606937e5ea0
Franco Androetto <androettofranco@gmail.com>
2023-10-02 20:44:39 -0300

    build: add api storage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   api: "https://api.poscloud.com.ar",
   apiv2: "https://apiv8.poscloud.com.ar",
-  apiPrint: "https://api-print.us-3.evennode.com"
+  apiPrint: "https://api-print.us-3.evennode.com",
+  apiStorage: "https://api-storage.us-3.evennode.com/"
 };
