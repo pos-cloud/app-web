b07a2eb678dd4b35156b86bd4af9c8652c38f534
Franco Androetto <androettofranco@gmail.com>
2023-12-26 23:24:14 -0300

    feat: rollback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -202,8 +202,11 @@ export class AppComponent {
         Config.setApiHost('http://' + hostname + ':300'); // LOCAL
         Config.setApiV8Host('http://' + hostname + ':308'); // LOCAL
       } else {
-        Config.setApiHost('https://api-v1.poscloud.ar'); // DONWEB
-        Config.setApiV8Host('https://api-v2.poscloud.ar'); // DONWEB
+        Config.setApiHost('https://api.poscloud.com.ar'); // DONWEB
+        Config.setApiV8Host('https://apiv8.poscloud.com.ar'); // DONWEB
+
+        // Config.setApiHost('https://api-v1.poscloud.ar'); // DONWEB
+        // Config.setApiV8Host('https://api-v2.poscloud.ar'); // DONWEB
       }
     } else {
         Config.setApiHost(environment.api); // TEST
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
-  api: "https://api-v1.poscloud.ar",
-  apiv2: "https://api-v2.poscloud.ar",
+  // api: "https://api-v1.poscloud.ar",
+  // apiv2: "https://api-v2.poscloud.ar",
+  api: "https://api.poscloud.com.ar",
+  apiv2: "https://apiv8.poscloud.com.ar",
   apiPrint: "https://api-print.poscloud.ar",
   apiStorage: "https://api-storage.poscloud.ar",
   apiTiendaNube: "https://api-tiendanube.poscloud.ar"
