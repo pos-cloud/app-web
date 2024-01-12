0573f16ff3a38e456f833574055755da7d6a9097
Franco Androetto <androettofranco@gmail.com>
2024-01-12 20:26:35 -0300

    build: add storage and print

--- services/print/app/app.ts
+++ services/print/app/app.ts
@@ -1,8 +1,7 @@
-import { json, urlencoded } from 'express';
-const morgan = require('morgan')
-import Routes from './routes/index';
-const cors = require('cors')
-
+import { json, urlencoded } from "express";
+const morgan = require("morgan");
+import Routes from "./routes/index";
+const cors = require("cors");
 
 export default class App {
   constructor(app: any) {
@@ -10,17 +9,10 @@ export default class App {
     new Routes(app);
   }
 
-  public config(app:any): void {
+  public config(app: any): void {
     app.use(json());
-    // const accessLogStream: WriteStream = fs.createWriteStream(
-    //   path.join(__dirname, './logs/access.log'),
-    //   { flags: 'a' }
-    // );
-     app.use(morgan('dev'));
-     app.use(urlencoded({ extended: true }));
-     app.use(cors());
-    //app.use(helmet());
-    //app.use(rateLimiter()); //  apply to all requests
-    ///app.use(unCoughtErrorHandler);
+    app.use(morgan("dev"));
+    app.use(urlencoded({ extended: true }));
+    app.use(cors());
   }
 }
