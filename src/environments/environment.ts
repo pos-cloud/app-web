679ce719c5c3e4d71e93d3ce3645404cf4db8b9f
Franco Androetto <androettofranco@gmail.com>
2024-01-14 17:24:30 -0300

    build: revert URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 
 export const environment = {
   production: false,
-  api: "https://d-api-v1.poscloud.ar",
-  apiv2: "https://d-api-v2.poscloud.ar",
-  apiPrint: "https://d-api-print.poscloud.ar",
-  apiStorage: "https://d-api-storage.poscloud.ar", 
-  apiTiendaNube: "https://d-api-tiendanube.poscloud.ar"
+  api: "http://localhost:300",
+  apiv2: "http://localhost:308",
+  apiPrint: "http://localhost:302",
+  apiStorage: "http://localhost:303", 
+  apiTiendaNube: "http://localhost:305"
 };
