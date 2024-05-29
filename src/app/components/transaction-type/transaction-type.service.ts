7290597169e12ed4cc2c532d3948bef495e18274
Jeremiasvaok <jeremias.vallejos78@gmail.com>
2024-05-29 19:12:51 -0300

    fix: search type

--- src/app/components/transaction-type/transaction-type.service.ts
+++ src/app/components/transaction-type/transaction-type.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
 import { ModelService } from '../model/model.service';
 import { AuthService } from '../login/auth.service';
-import { Observable } from "rxjs";
+import { Observable, of} from "rxjs";
+import {HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
+import {Config} from '../../app.config';
+import {map, catchError} from 'rxjs/operators';
 
 @Injectable()
 export class TransactionTypeService extends ModelService {
@@ -24,4 +26,28 @@ export class TransactionTypeService extends ModelService {
     public getJSON(): Observable<any> {
         return this._http.get(this._jsonURL);
     }
+
+    public getTrasactionTypes(query?: string): Observable<any> {
+        const URL = `${Config.apiURL}transaction-types`;
+    
+        const headers = new HttpHeaders()
+          .set('Content-Type', 'application/json')
+          .set('Authorization', this._authService.getToken());
+    
+        const params = new HttpParams().set('query', query);
+    
+        return this._http
+          .get(URL, {
+            headers: headers,
+            params: params,
+          })
+          .pipe(
+            map((res) => {
+              return res;
+            }),
+            catchError((err) => {
+              return of(err);
+            }),
+          );
+      }
 }
