d8469c74d8a3148f6483e8439e2c9a44abd00a9a
Franco Androetto <androettofranco@gmail.com>
2024-08-07 11:15:25 +0200

    build: test for new prod

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,13 @@
 export const environment = {
     production: false,
-    api: "https://d-api-v1.poscloud.ar",
-    apiv2: "https://d-api-v2.poscloud.ar",
-    apiPrint: "https://d-api-print.poscloud.ar",
-    apiStorage: "https://d-api-storage.poscloud.ar", 
-    apiTiendaNube: "https://d-api-tiendanube.poscloud.ar"
+    // api: "https://d-api-v1.poscloud.ar",
+    // apiv2: "https://d-api-v2.poscloud.ar",
+    // apiPrint: "https://d-api-print.poscloud.ar",
+    // apiStorage: "https://d-api-storage.poscloud.ar", 
+    // apiTiendaNube: "https://d-api-tiendanube.poscloud.ar",
+    api: "http://149.50.141.176:300",
+    apiv2: "http://149.50.141.176:308",
+    apiPrint: "http://149.50.141.176:302",
+    apiStorage: "http://149.50.141.176:303", 
+    apiTiendaNube: "http://149.50.141.176:305"
 };
