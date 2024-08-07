0c3e75c13ab789893233cad5aafcf6c129325fb0
Franco Androetto <androettofranco@gmail.com>
2024-08-07 11:46:01 +0200

    build: revert dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,13 +1,8 @@
 export const environment = {
     production: false,
-    // api: "https://d-api-v1.poscloud.ar",
-    // apiv2: "https://d-api-v2.poscloud.ar",
-    // apiPrint: "https://d-api-print.poscloud.ar",
-    // apiStorage: "https://d-api-storage.poscloud.ar", 
-    // apiTiendaNube: "https://d-api-tiendanube.poscloud.ar",
-    api: "https://vps-4209130-x.dattaweb.com:300",
-    apiv2: "https://vps-4209130-x.dattaweb.com:308",
-    apiPrint: "https://vps-4209130-x.dattaweb.com:302",
-    apiStorage: "https://vps-4209130-x.dattaweb.com:303", 
-    apiTiendaNube: "https://vps-4209130-x.dattaweb.com:305"
+    api: "https://d-api-v1.poscloud.ar",
+    apiv2: "https://d-api-v2.poscloud.ar",
+    apiPrint: "https://d-api-print.poscloud.ar",
+    apiStorage: "https://d-api-storage.poscloud.ar", 
+    apiTiendaNube: "https://d-api-tiendanube.poscloud.ar",
 };
