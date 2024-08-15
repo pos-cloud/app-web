38dfc78a495eb2d21d0b1290f1950105ec429fe6
Franco Androetto <androettofranco@gmail.com>
2024-08-15 16:20:28 +0200

    build: test pass URL from script nodejs

--- services/api-v2/src/utils/config.ts
+++ services/api-v2/src/utils/config.ts
@@ -6,7 +6,7 @@ export default {
   LIC_PASSWORD: process.env.TOKEN_SECRET || 'P05R35T@L1C',
   MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/',
   URL_SERVER: 'http://vps-1883265-x.dattaweb.com',
-  API_URL: process.env.API_URL || 'https://api-v1.poscloud.ar/api',
+  API_URL: process.env.API_URL || 'http://localhost:300/api',
   FORGOT_PASSWORD_EXPIRATION_TIME: 120,
   SESSION_EXPIRATION_TIME: 30,
   API_URL_FE_AR: 'http://vps-1883265-x.dattaweb.com/libs/fe/ar/index.php',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
   api: "http://localhost:300",
   apiv2: "http://localhost:308",
   apiPrint: "http://localhost:302",
-  apiStorage: "http://localhost:303", 
+  apiStorage: "https://api-minio.poscloud.ar",
   apiTiendaNube: "http://localhost:305"
 };
