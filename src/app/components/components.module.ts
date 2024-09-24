6aaba644f0ce72ff252d589db42198ca1b1a6970
Franco Androetto <androettofranco@gmail.com>
2024-09-24 19:36:47 +0200

    feat: export artcle taxes

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,27 +1,27 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {AccountPeriodModule} from './account-period/account-period.module';
-import {AccountSeatModule} from './account-seat/account-seat.module';
-import {AccountModule} from './account/account.module';
-import {ApplicationModule} from './application/application.module';
-import {BusinessRuleModule} from './business-rules/business-rule.module';
-import {CashBoxTypeModule} from './cash-box-type/cash-box-type.module';
-import {CategoryModule} from './category/category.module';
-import {CompanyGroupModule} from './company-group/company-group.module';
-import {DatatableModule} from './datatable/datatable.module';
-import {EmployeeTypeModule} from './employee-type/employee-type.module';
-import {ExportersModule} from './export/exporters.module';
-import {HistoryModule} from './history/history.module';
-import {HolidayModule} from './holiday/holiday.module';
-import {PermissionModule} from './permission/permission.module';
-import {ReportModule} from './report/report.module';
-import {ShipmentMethodModule} from './shipment-method/shipment-method.module';
-import {TransactionTypeModule} from './transaction-type/transaction-type.module';
-import {UnitOfMeasurementModule} from './unit-of-measurement/unit-of-measurement.module';
-import {VariantValueModule} from './variant-value/variant-value.module';
-import { ReportsModule } from './reports/reports.module';
-import {ArticleModule} from './article/article.module'
+import { AccountPeriodModule } from './account-period/account-period.module';
+import { AccountSeatModule } from './account-seat/account-seat.module';
+import { AccountModule } from './account/account.module';
+import { ApplicationModule } from './application/application.module';
+import { ArticleModule } from './article/article.module';
+import { BusinessRuleModule } from './business-rules/business-rule.module';
+import { CashBoxTypeModule } from './cash-box-type/cash-box-type.module';
+import { CategoryModule } from './category/category.module';
+import { CompanyGroupModule } from './company-group/company-group.module';
+import { DatatableModule } from './datatable/datatable.module';
+import { EmployeeTypeModule } from './employee-type/employee-type.module';
+import { ExportersModule } from './export/exporters.module';
+import { HistoryModule } from './history/history.module';
+import { HolidayModule } from './holiday/holiday.module';
 import { MakeModule } from './make/make.module';
+import { PermissionModule } from './permission/permission.module';
+import { ReportModule } from './report/report.module';
+import { ReportsModule } from './reports/reports.module';
+import { ShipmentMethodModule } from './shipment-method/shipment-method.module';
+import { TransactionTypeModule } from './transaction-type/transaction-type.module';
+import { UnitOfMeasurementModule } from './unit-of-measurement/unit-of-measurement.module';
+import { VariantValueModule } from './variant-value/variant-value.module';
 
 @NgModule({
   imports: [
@@ -46,8 +46,9 @@ import { MakeModule } from './make/make.module';
     BusinessRuleModule,
     ReportsModule,
     ArticleModule,
-    MakeModule
+    MakeModule,
   ],
+  exports: [ArticleModule],
   declarations: [],
 })
 export class ComponentsModule {}
