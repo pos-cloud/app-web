fed9527f28c2612ea138eb0f3f7362a9b22fb355
Franco Androetto <androettofranco@gmail.com>
2024-12-06 11:29:20 -0300

    ⚡ perf: import module entities

--- src/app/entities/gallery/gallery.module.ts
+++ src/app/entities/gallery/gallery.module.ts
@@ -1,14 +1,5 @@
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule } from '@ngx-translate/core';
-import { DatatableModule } from 'app/components/datatable/datatable.module';
-import { FocusDirective } from 'app/shared/directives/focus.directive';
-import { PipesModule } from 'app/shared/pipes/pipes.module';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { ProgressbarModule } from '../../shared/components/progressbar/progressbar.module';
+import { EntitiesModule } from '../entities.module';
 import { GalleryComponent } from './crud/gallery.component';
 import { GalleryRoutingModule } from './gallery-routing.module';
 import { ListGalleriesComponent } from './list-galleries/list-galleries.component';
@@ -20,21 +11,7 @@ import { ViewGalleryComponent } from './view-gallery/view-gallery.component';
     ListGalleriesComponent,
     ViewGalleryComponent,
   ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NgxPaginationModule,
-    FocusDirective,
-    DragDropModule,
-    ProgressbarModule,
-    PipesModule,
-    TranslateModule,
-    NgbDropdownModule,
-    NgbModule,
-    DatatableModule,
-    GalleryRoutingModule,
-  ],
+  imports: [EntitiesModule, GalleryRoutingModule],
   providers: [],
 })
 export class GalleryModule {}
--- src/app/entities/make/make.module.ts
+++ src/app/entities/make/make.module.ts
@@ -1,34 +1,11 @@
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule } from '@ngx-translate/core';
-import { DatatableModule } from 'app/components/datatable/datatable.module';
-import { FocusDirective } from 'app/shared/directives/focus.directive';
-import { PipesModule } from 'app/shared/pipes/pipes.module';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { ProgressbarModule } from '../../shared/components/progressbar/progressbar.module';
+import { EntitiesModule } from '../entities.module';
 import { MakeComponent } from './crud/make.component';
 import { ListMakesComponent } from './list/list-makes.component';
 import { MakeRoutingModule } from './make-routing.module';
 
 @NgModule({
   declarations: [MakeComponent, ListMakesComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NgxPaginationModule,
-    FocusDirective,
-    DragDropModule,
-    ProgressbarModule,
-    PipesModule,
-    TranslateModule,
-    NgbDropdownModule,
-    NgbModule,
-    DatatableModule,
-    MakeRoutingModule,
-  ],
+  imports: [EntitiesModule, MakeRoutingModule],
 })
 export class MakeModule {}
