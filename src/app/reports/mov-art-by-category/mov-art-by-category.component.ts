3c352d6bf268baee5a3f2704ddcccac9b31220d1
Franco Androetto <androettofranco@gmail.com>
2025-02-02 05:51:15 -0300

    initial

--- src/app/reports/mov-art-by-category/mov-art-by-category.component.ts
+++ src/app/reports/mov-art-by-category/mov-art-by-category.component.ts
@@ -190,7 +190,7 @@ export class ReportMovArtByCategoryComponent implements OnInit {
 
     this.subscription.add(
       this._service
-        .getReport(requestPayload)
+        .getReportSystem(requestPayload)
         .pipe(takeUntil(this.destroy$))
         .subscribe({
           next: (result) => {
