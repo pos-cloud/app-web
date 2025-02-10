41ed026fc81e0e361b11fb84a97939c4ffc7f920
Franco Androetto <androettofranco@gmail.com>
2025-02-10 17:07:12 -0300

    feat: add mov art by art

--- src/app/reports/mov-art-by-article/mov-art-by-article.component.ts
+++ src/app/reports/mov-art-by-article/mov-art-by-article.component.ts
@@ -1,14 +1,174 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { Branch } from '@types';
+import { TransactionType } from 'app/components/transaction-type/transaction-type';
+import { BranchService } from 'app/core/services/branch.service';
+import { ReportSystemService } from 'app/core/services/report-system.service';
+import { TransactionTypeService } from 'app/core/services/transaction-type.service';
+import { DataTableReportsComponent } from 'app/shared/components/data-table-reports/data-table-reports.component';
+import { DateTimePickerComponent } from 'app/shared/components/datetime-picker/date-time-picker.component';
+import { MultiSelectDropdownComponent } from 'app/shared/components/multi-select-dropdown/multi-select-dropdown.component';
+import { ToastService } from 'app/shared/components/toast/toast.service';
+import { PipesModule } from 'app/shared/pipes/pipes.module';
+import { Subject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-mov-art-by-article',
   templateUrl: './mov-art-by-article.component.html',
   encapsulation: ViewEncapsulation.None,
   standalone: true,
-  imports: [CommonModule, NgbModule, NgMultiSelectDropDownModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TranslateModule,
+    PipesModule,
+    DateTimePickerComponent,
+    MultiSelectDropdownComponent,
+    DataTableReportsComponent,
+    ReactiveFormsModule,
+  ],
 })
-export class ReportMovArtByArticleComponent {}
+export class ReportMovArtByArticleComponent {
+  // date table
+  public data: any[] = [];
+  public columns: any[] = [];
+  public totals: any = {};
+
+  public transactionMovement: string;
+  public loading: boolean = false;
+  private destroy$ = new Subject<void>();
+  private subscription: Subscription = new Subscription();
+
+  // filters
+
+  transactionTypes: TransactionType[];
+  transactionTypesSelect: string[] = [];
+
+  branches: Branch[];
+  branchSelectedId: string[] = [];
+
+  startDate: string = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toISOString();
+  endDate: string = new Date().toISOString();
+
+  constructor(
+    private _service: ReportSystemService,
+    private _branchService: BranchService,
+    private _transactionTypeService: TransactionTypeService,
+    private _toastService: ToastService,
+    private _activatedRoute: ActivatedRoute
+  ) {}
+
+  async ngOnInit() {
+    this._activatedRoute.params.subscribe((params) => {
+      this.transactionMovement = params['module'].charAt(0).toUpperCase() + params['module'].slice(1);
+    });
+
+    this.getBranches();
+    this.getTransactionTypes();
+    this.getReport();
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private getBranches(): Promise<Branch[]> {
+    return new Promise<Branch[]>((resolve, reject) => {
+      this._branchService
+        .getAll({
+          project: {
+            _id: 1,
+            operationType: 1,
+            name: 1,
+          },
+          match: {
+            operationType: { $ne: 'D' },
+          },
+        })
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (result) => {
+            this.branches = result.result;
+          },
+          error: (error) => {
+            resolve(null);
+          },
+          complete: () => {},
+        });
+    });
+  }
+
+  private getTransactionTypes() {
+    this._transactionTypeService
+      .getAll({
+        project: {
+          _id: 1,
+          transactionMovement: 1,
+          requestArticles: 1,
+          operationType: 1,
+          name: 1,
+          branch: 1,
+        },
+        match: {
+          transactionMovement: this.transactionMovement,
+          operationType: { $ne: 'D' },
+        },
+      })
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (result) => {
+          this.transactionTypes = result.result;
+        },
+        error: (error) => {
+          this._toastService.showToast(error);
+        },
+      });
+  }
+
+  public getReport(): void {
+    this.loading = true;
+
+    const requestPayload = {
+      reportType: 'mov-art-by-article',
+      filters: {
+        branch: this.branchSelectedId,
+        type: this.transactionMovement,
+        transactionTypes: this.transactionTypesSelect ?? [],
+        startDate: this.startDate,
+        endDate: this.endDate,
+      },
+      pagination: {
+        page: 1,
+        pageSize: 10,
+      },
+      sorting: {
+        column: 'description',
+        direction: 'asc',
+      },
+    };
+
+    this.subscription.add(
+      this._service
+        .getReport(requestPayload)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (result) => {
+            this._toastService.showToast(result);
+            this.data = result.result.data;
+            this.columns = result.result.columns;
+            this.totals = result.result.totals;
+          },
+          error: (error) => {
+            this._toastService.showToast(error);
+          },
+          complete: () => {
+            this.loading = false;
+          },
+        })
+    );
+  }
+}
