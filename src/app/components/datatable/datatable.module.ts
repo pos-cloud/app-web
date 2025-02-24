e04ce008347ba06be68d73675422b81f880ccda8
Franco Androetto <androettofranco@gmail.com>
2025-02-24 07:35:44 -0300

    ⚡ perf: remove unnecessary imports

--- src/app/components/datatable/datatable.module.ts
+++ src/app/components/datatable/datatable.module.ts
@@ -2,11 +2,9 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
-import { FocusDirective } from 'app/shared/directives/focus.directive';
 import { PipesModule } from 'app/shared/pipes/pipes.module';
-import { NgxPaginationModule } from 'ngx-pagination'; // https://www.npmjs.com/package/ngx-pagination
 import { ProgressbarModule } from '../../shared/components/progressbar/progressbar.module';
 import { ExportersModule } from '../export/exporters.module';
 import { DatatableComponent } from './datatable.component';
@@ -16,15 +14,12 @@ import { DatatableComponent } from './datatable.component';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    NgxPaginationModule,
-    FocusDirective,
     ExportersModule,
     DragDropModule,
     TranslateModule,
     ProgressbarModule,
     PipesModule,
     NgbModule,
-    NgbDropdownModule,
   ],
   exports: [DatatableComponent],
   declarations: [DatatableComponent],
