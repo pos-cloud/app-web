106bb65e7cd7becafeb01fa9ac4defb9710adb01
Matias Olmos <matiasolmosrivero@gmail.com>
2025-03-17 16:01:44 -0300

    Added function to handle click on toast service

--- src/app/shared/components/toast/toast.service.ts
+++ src/app/shared/components/toast/toast.service.ts
@@ -1,7 +1,11 @@
 import { Injectable, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class ToastService {
+  constructor(
+    private _router: Router
+  ) {}
   toasts: any[] = [];
 
   // Muestra una notificación
@@ -14,20 +18,26 @@ export class ToastService {
     this.toasts = this.toasts.filter((t) => t !== toast);
   }
 
+  handleClick(toast) {
+    if (toast.redirect !== ''){
+      this.remove(toast)
+      this._router.navigate([toast.redirect])
+    }else{
+      this.remove(toast);
+    }
+  }
+
   // Métodos para notificaciones específicas
-  private success(message: string, title: string) {
-    this.show(message, {
-      classname: 'bg-success text-light',
-      header: title,
-    });
+  private success(message: string, title: string, redirect: string) {
+    this.show(message, { classname: 'bg-success text-light', header: title , redirect });
   }
 
-  private error(message: string, title: string) {
-    this.show(message, { classname: 'bg-danger text-light', header: title });
+  private error(message: string, title: string, redirect: string) {
+    this.show(message, { classname: 'bg-danger text-light', header: title , redirect });
   }
 
-  private info(message: string, title: string) {
-    this.show(message, { classname: 'bg-info text-light', header: title });
+  private info(message: string, title: string, redirect: string) {
+    this.show(message, { classname: 'bg-info text-light', header: title , redirect });
   }
 
   // Método centralizado para manejar toasts
@@ -35,31 +45,37 @@ export class ToastService {
     result: any = null,
     type?: string,
     title: string = '',
-    message: string = ''
+    message: string = '',
+    redirect: string = ''
   ): void {
     if (result) {
       if (result.status === 200) {
         type = 'success';
         message = result.message;
+        redirect = result.redirect;
       } else if (result.status >= 400) {
         type = 'danger';
         message = result.error?.message || result.message;
+        redirect = result.redirect;
+
       } else {
         type = 'info';
         message = result.message;
+        redirect = result.redirect;
       }
     }
 
     switch (type) {
       case 'success':
-        this.success(message, title);
+        this.success(message, title, redirect);
         break;
       case 'danger':
-        this.error(message, title);
+        this.error(message, title, redirect);
         break;
       default:
-        this.info(message, title);
+        this.info(message, title, redirect);
         break;
     }
+
   }
 }
