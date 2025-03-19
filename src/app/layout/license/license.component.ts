a66b00c9c26cea64daae64777de4e4758e2c69b5
Matias Olmos <matiasolmosrivero@gmail.com>
2025-03-19 17:12:03 -0300

    Styling of the license component

--- src/app/layout/license/license.component.ts
+++ src/app/layout/license/license.component.ts
@@ -1,25 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ConfigService } from 'app/core/services/config.service';
 import { Config } from "app/app.config";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: 'app-license',
     templateUrl: './license.component.html',
+    styleUrls: ['./license.component.scss'],
     standalone: true,
+    imports: [CommonModule]
 })
-export class LicenseComponent {
+export class LicenseComponent implements OnInit{
     config: Config;
+    licensePaymentDueDate: String;
+    expirationLicenseDate: String;
+    licenseType: Number;
 
     constructor(
         private _configService: ConfigService
     ) {
+        
+    }
+
+    ngOnInit(): void {
         this._configService.getConfig.subscribe((config) => {
             this.config = config;
+            this.expirationLicenseDate = config.expirationLicenseDate; //preguntar por que no funciona con this ni fuera del subscribe.
+            this.licensePaymentDueDate = config.licensePaymentDueDate; //preguntar por que no funciona con this ni fuera del subscribe.
         });
-
-        console.log(this.config)
-    };
-
-
-
+        this.licenseType = 1 
+    }
 }
