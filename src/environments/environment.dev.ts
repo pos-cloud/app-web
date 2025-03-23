3c4082de8750a1cb53fcdce5aa9b241d954c871c
Franco Androetto <androettofranco@gmail.com>
2025-03-23 15:36:59 -0300

    ✨ feat: add get img from api-core

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,9 +1,8 @@
 export const environment = {
-    production: false,
-    api: "https://d-api-v1.poscloud.ar",
-    apiv2: "https://d-api-v2.poscloud.ar",
-    apiPrint: "https://d-api-print.poscloud.ar",
-    apiStorage: "https://d-api-storage.poscloud.ar", 
-    apiTiendaNube: "https://d-api-tiendanube.poscloud.ar",  
-    feAr: "https://d-fe-ar.poscloud.ar"
+  production: false,
+  api: 'https://d-api-v1.poscloud.ar',
+  apiv2: 'https://d-api-v2.poscloud.ar',
+  apiStorage: 'https://d-api-storage.poscloud.ar',
+  apiTiendaNube: 'https://d-api-tiendanube.poscloud.ar',
+  feAr: 'https://d-fe-ar.poscloud.ar',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,8 @@
 export const environment = {
   production: true,
-  api: "https://api-v1.poscloud.ar",
-  apiv2: "https://api-v2.poscloud.ar",
-  apiPrint: "https://api-print.poscloud.ar",
-  apiStorage: "https://api-storage.poscloud.ar",
-  apiTiendaNube: "https://api-tiendanube.poscloud.ar",
-  feAr: "https://fe-ar.poscloud.ar"
+  api: 'https://api-v1.poscloud.ar',
+  apiv2: 'https://api-v2.poscloud.ar',
+  apiStorage: 'https://api-storage.poscloud.ar',
+  apiTiendaNube: 'https://api-tiendanube.poscloud.ar',
+  feAr: 'https://fe-ar.poscloud.ar',
 };
