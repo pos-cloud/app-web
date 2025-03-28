6b363d0868bda054907b73953a358202c659f0f3
Matias Olmos <matiasolmosrivero@gmail.com>
2025-03-28 18:37:41 -0300

    Basic payment complete circuit

--- src/app/core/services/license.service.ts
+++ src/app/core/services/license.service.ts
@@ -3,12 +3,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 import { environment } from 'environments/environment';
 import { loadMercadoPago } from "@mercadopago/sdk-js";
+import { Router } from '@angular/router';
 
 
 declare global {
   interface Window {
     MercadoPago: any;
-    deviceId?: string;
   }
 }
 
@@ -19,18 +19,17 @@ export class MercadoPagoService {
   private mp: any;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router,
   ) {
   }
 
   async initializeMercadoPago() {
     await loadMercadoPago();
 
-    //preguntar por el .ENV para las keys de MercadoPago
-    this.mp = new window.MercadoPago("aca va la MercadoPagoPublicKey ", {
+    this.mp = new window.MercadoPago("APP_USR-3e6fe769-4628-4c46-b2f8-7a472fc09e70", {
       locale: "es-AR",
     });
-
   }
 
   async createPreference(): Promise<any> {
@@ -47,7 +46,6 @@ export class MercadoPagoService {
   }
 
   prepareLicenseData() {
-    //acá llamaría a api license para traer los datos actualizados de la licencia, por ahora hardcodeo unos datos
     let licenseData = {
       id: "1",
       title: "Plan Basico",
@@ -61,6 +59,7 @@ export class MercadoPagoService {
   async createPaymentBrick(containerId: string, payer: object) {
     await this.initializeMercadoPago();
 
+
     if (!this.mp) {
       console.error('MercadoPago no inicializado');
       return;
@@ -68,16 +67,15 @@ export class MercadoPagoService {
 
     const licenseData = this.prepareLicenseData();
     const amount = licenseData.unit_price;
-    const bricksBuilder = this.mp.bricks();
-    const preferenceId = await this.createPreference();
-    console.log(licenseData);
+    const bricksBuilder = await this.mp.bricks();
+    const preference = await this.createPreference();
+    const preferenceId = preference.preferenceId;
     try {
       await bricksBuilder.create("payment", containerId, {
         initialization: {
           amount,
           preferenceId,
           payer,
-          capture: true,
         },
         customization: {
           visual: {
@@ -86,19 +84,31 @@ export class MercadoPagoService {
           paymentMethods: {
             creditCard: "all",
             debitCard: "all",
-            ticket: "all",
-            bankTransfer: "all",
-            atm: "all",
-            onboarding_credits: "all",
-            wallet_purchase: "all",
+            mercadoPago: "all",
             maxInstallments: 1,
           },
         },
         callbacks: {
           onReady: () => console.log("Payment Brick listo."),
           onSubmit: async ({ formData }: { formData: any }) => {
-            console.log(formData);
+            try {
+              console.log(formData);
+              const headers = new HttpHeaders({
+                "Content-Type": "application/json",
+              });
 
+              const data = await firstValueFrom(
+                this.http.post(
+                  `${environment.apiLicense}/payments/create-payment`,
+                  formData,
+                  { headers }
+                )
+              );
+              console.log("Pago procesado:", data);
+              this.router.navigate(['/'])
+            } catch (error: any) {
+              console.error("Error en el pago:", error?.error || error?.message);
+            }
           },
           onError: (error: any) =>
             console.error("Error en Payment Brick:", error),
