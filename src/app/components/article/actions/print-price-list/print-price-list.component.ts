b5be802dc72e4c289150ad835ab5cc3e28b50baa
Franco Androetto <androettofranco@gmail.com>
2025-04-09 13:33:55 -0300

    ♻️ refactor: print price list

--- src/app/components/article/actions/print-price-list/print-price-list.component.ts
+++ src/app/components/article/actions/print-price-list/print-price-list.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -10,108 +10,79 @@ import { Category } from '../../../category/category';
 //service
 import { CommonModule } from '@angular/common';
 import { PrintService } from '@core/services/print.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { MultiSelectDropdownComponent } from '@shared/components/multi-select-dropdown/multi-select-dropdown.component';
 import { ToastService } from '@shared/components/toast/toast.service';
+import { PipesModule } from '@shared/pipes/pipes.module';
 import { PriceList } from 'app/components/price-list/price-list';
 import { MakeService } from 'app/core/services/make.service';
 import { PriceListService } from 'app/core/services/price-list.service';
 import * as printJS from 'print-js';
-import { Subject, takeUntil } from 'rxjs';
+import { combineLatest, Subject, takeUntil } from 'rxjs';
 import { CategoryService } from '../../../../core/services/category.service';
 
 @Component({
   selector: 'app-print-price-list',
   templateUrl: './print-price-list.component.html',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, PipesModule, TranslateModule, MultiSelectDropdownComponent],
 })
-export class PrintPriceListComponent {
+export class PrintPriceListComponent implements OnInit {
   private destroy$ = new Subject<void>();
 
-  public printPriceListForm: UntypedFormGroup;
   public loading: boolean = false;
-  public makes: Make;
-  public categories: Category;
+  public makes: Make[];
+  public categories: Category[];
   public priceLists: PriceList[];
 
+  public filters = {
+    makes: [],
+    categories: [],
+    priceList: '',
+    image: 'false',
+  };
+
   constructor(
-    private _fb: UntypedFormBuilder,
     public activeModal: NgbActiveModal,
     private _makeService: MakeService,
     private _categoryService: CategoryService,
-    private _priceList: PriceListService,
+    private _priceListService: PriceListService,
     private _printService: PrintService,
     private _toastService: ToastService
-  ) {
-    this.getMakes();
-    this.getCategories();
-    this.getPriceLists();
-    this.buildForm();
-  }
+  ) {}
 
-  public getMakes(): void {
-    this.loading = true;
-
-    this._makeService.getMakes('sort="description":1').subscribe(
-      (result) => {
-        if (!result.makes) {
-        } else {
-          this.makes = result.makes;
-        }
-      },
-      (error) => {
-        this.loading = false;
-      }
-    );
-  }
-
-  public getCategories(): void {
-    this.loading = true;
-
-    this._categoryService.getCategories('sort="description":1').subscribe(
-      (result) => {
-        if (!result.categories) {
-        } else {
-          this.categories = result.categories;
-        }
-        this.loading = false;
-      },
-      (error) => {
-        this.loading = false;
-      }
-    );
-  }
-
-  public getPriceLists(): void {
-    this.loading = true;
-
-    this._priceList.getPriceLists().subscribe(
-      (result) => {
-        if (!result.priceLists) {
-        } else {
-          this.priceLists = result.priceLists;
-        }
-        this.loading = false;
-      },
-      (error) => {
-        this.loading = false;
-      }
-    );
-  }
-
-  public buildForm(): void {
-    this.printPriceListForm = this._fb.group({
-      make: ['', []],
-      category: ['', []],
-      withImage: [false, []],
-      priceList: ['', []],
-    });
+  ngOnInit(): void {
+    combineLatest({
+      categories: this._categoryService.find({ query: { operationType: { $ne: 'D' } } }),
+      makes: this._makeService.find({ query: { operationType: { $ne: 'D' } } }),
+      priceLists: this._priceListService.find({ query: { operationType: { $ne: 'D' } } }),
+    })
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: ({ categories, makes, priceLists }) => {
+          this.categories = categories || [];
+          this.makes = makes || [];
+          this.priceLists = priceLists || [];
+        },
+        error: (error) => {
+          this._toastService.showToast(error);
+        },
+        complete: () => {
+          this.loading = false;
+        },
+      });
   }
 
   public toPrint(): void {
     this.loading = true;
 
+    const filters = {
+      ...this.filters,
+      image: this.filters.image == 'true' ? true : false,
+    };
+
     this._printService
-      .toPrint(PrintType.PriceList, this.printPriceListForm.value)
+      .toPrint(PrintType.PriceList, filters)
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (result: Blob | ApiResponse) => {
