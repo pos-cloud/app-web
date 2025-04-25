6edf6138b7928683626e787cb29f0ab16a4440e1
Franco Androetto <androettofranco@gmail.com>
2025-04-25 13:30:11 -0300

    🎨 style: remove granpaso

--- src/environments/environment.granpaso.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  api: 'http://192.168.88.90:300',
-  apiv2: 'http://192.168.88.90:308',
-  apiStorage: 'https://api-storage.poscloud.ar',
-  apiTiendaNube: 'https://api-tiendanube.poscloud.ar',
-  feAr: 'https://fe-ar.poscloud.ar',
-};
