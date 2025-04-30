29d013dc3bc2d1eeb7f544a75a4ab9e3efdae662
Franco Androetto <androettofranco@gmail.com>
2025-04-30 16:03:43 -0300

    🎨 style: remove service

--- src/app/core/services/send-email.service.ts
+++ src/app/core/services/send-email.service.ts
@@ -12,10 +12,7 @@ import { AuthService } from './auth.service';
   providedIn: 'root',
 })
 export class EmailService {
-  constructor(
-    private _http: HttpClient,
-    private _authService: AuthService
-  ) {}
+  constructor(private _http: HttpClient, private _authService: AuthService) {}
 
   public sendEmail(data: EmailProps): Observable<any> {
     const URL = `${Config.apiURL}send-email-client`;
@@ -28,67 +25,6 @@ export class EmailService {
       .post(URL, data, {
         headers: headers,
       })
-      .pipe(
-        map((res) => {
-          return res;
-        }),
-        catchError((err) => {
-          return of(err);
-        })
-      );
-  }
-
-  public sendEmailToClient(subject: string, message: string): Observable<any> {
-    const URL = `${Config.apiURL}send-email-to-client`;
-
-    const headers = new HttpHeaders()
-      .set('Content-Type', 'application/json')
-      .set('Authorization', this._authService.getToken());
-
-    return this._http
-      .post(
-        URL,
-        {
-          subject: subject,
-          message: message,
-        },
-        {
-          headers: headers,
-        }
-      )
-      .pipe(
-        map((res) => {
-          return res;
-        }),
-        catchError((err) => {
-          return of(err);
-        })
-      );
-  }
-
-  public sendEmailClient(
-    subject: string,
-    body: string,
-    emails: string
-  ): Observable<any> {
-    const URL = `${Config.apiURL}send-email-client`;
-
-    const headers = new HttpHeaders()
-      .set('Content-Type', 'application/json')
-      .set('Authorization', this._authService.getToken());
-
-    return this._http
-      .post(
-        URL,
-        {
-          subject: subject,
-          body: body,
-          emails: emails,
-        },
-        {
-          headers: headers,
-        }
-      )
       .pipe(
         map((res) => {
           return res;
