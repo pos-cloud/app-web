d80cbda6cc469c55a1b08a092a3e180a939fc983
Franco Androetto <androettofranco@gmail.com>
2025-06-13 08:29:10 -0300

    ✨ feat: lic

--- src/app/core/guards/notification.guard.ts
+++ src/app/core/guards/notification.guard.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 import { Config } from 'app/app.config';
 import { ConfigService } from 'app/core/services/config.service';
 import { ToastService } from 'app/shared/components/toast/toast.service';
@@ -14,40 +10,34 @@ import { map, take } from 'rxjs/operators';
 export class NotificationGuard implements CanActivate {
   public config: Config;
 
-  constructor(
-    private _configService: ConfigService,
-    private _toastService: ToastService
-  ) {}
+  constructor(private _configService: ConfigService, private _toastService: ToastService) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean> {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return this._configService.getConfig.pipe(
       take(1),
       map((config: Config) => {
         if (config) {
-          if (config["expirationLicenseDate"]) {
-            const dueDate = new Date(config["expirationLicenseDate"]);
+          if (config['expirationLicenseDate']) {
+            const dueDate = new Date(config['expirationLicenseDate']);
             const today = new Date();
-            
+
             dueDate.setHours(0, 0, 0, 0);
             today.setHours(0, 0, 0, 0);
-            
+
             const timeDiff = dueDate.getTime() - today.getTime();
             let days = Math.floor(timeDiff / (1000 * 3600 * 24)) + 1;
-  
+
             let message: string;
             let toastType: string;
             let toastDelay: number;
-  
-            if (!config["demo"]) {
+
+            if (!config['demo']) {
               const settings = this._toastService.calculateNotificationSettings(days);
               toastType = settings.type;
               toastDelay = settings.delay;
-            
+
               if (days <= 0) {
-                message = 'Su licencia expiró, por favor, regularice su pago.';
+                message = 'Su licencia ha expirado. Por favor, regularice su pago.';
               } else if (days === 1) {
                 message = 'Su licencia vence hoy.';
               } else if (days <= 5) {
@@ -55,16 +45,16 @@ export class NotificationGuard implements CanActivate {
               } else if (days <= 10) {
                 message = `Su licencia vence en ${days} días.`;
               }
-  
+
               if (message) {
                 this._toastService.showToast(
                   null,
                   toastType,
-                  "",
-                  message,
-                  "/license",
+                  'Licencia',
+                  `${message}`,
+                  '/license',
                   toastDelay,
-                  "bottom-right"
+                  'bottom-right'
                 );
               }
             } else {
@@ -76,13 +66,13 @@ export class NotificationGuard implements CanActivate {
                 this._toastService.showToast({ message, type: 'info' });
               }
             }
-  
+
             if (message) {
               localStorage.setItem('notificationMessage', message);
             }
           }
         }
-        
+
         return true;
       })
     );
