eb0df068a8c27c54e25d679328fca99ddc37c7ee
Franco Androetto <androettofranco@gmail.com>
2025-07-02 06:46:18 -0300

    ✨ feat: upgrade permission

--- services/api-v1/models/permission.js
+++ services/api-v1/models/permission.js
@@ -1,66 +1,71 @@
-'use strict'
+'use strict';
 
-const { ObjectID } = require('bson')
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const { ObjectID } = require('bson');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const PermissionSchema = new Schema({
   name: { type: String, trim: true, required: true, unique: true },
-  collections: [
-    {
-      name: { type: String },
-      actions: {
-        view: { type: Boolean },
-        add: { type: Boolean },
-        edit: { type: Boolean },
-        delete: { type: Boolean },
-        export: { type: Boolean },
-      },
-      values: [
-        {
-          id: { type: ObjectID },
-        },
-      ],
+  collections: {
+    transactions: {
+      view: { type: Boolean },
+      add: { type: Boolean },
+      edit: { type: Boolean },
+      delete: { type: Boolean },
+      export: { type: Boolean },
     },
-  ],
+    articles: {
+      view: { type: Boolean },
+      add: { type: Boolean },
+      edit: { type: Boolean },
+      delete: { type: Boolean },
+      export: { type: Boolean },
+    },
+    companies: {
+      view: { type: Boolean },
+      add: { type: Boolean },
+      edit: { type: Boolean },
+      delete: { type: Boolean },
+      export: { type: Boolean },
+    },
+    movementsOfArticles: {
+      view: { type: Boolean },
+      add: { type: Boolean },
+      edit: { type: Boolean },
+      delete: { type: Boolean },
+      export: { type: Boolean },
+    },
+  },
   menu: {
     sales: {
       counter: { type: Boolean },
-      webOrders: { type: Boolean },
+      tiendaNube: { type: Boolean },
+      wooCommerce: { type: Boolean },
       delivery: { type: Boolean },
       voucherReader: { type: Boolean },
       resto: { type: Boolean },
     },
-    gallery: { type: Boolean },
     resto: { type: Boolean },
-    purchases: { type: Boolean },
-    production: { type: Boolean },
     money: { type: Boolean },
+    production: { type: Boolean },
+    purchases: { type: Boolean },
     stock: { type: Boolean },
     articles: { type: Boolean },
     companies: {
       client: { type: Boolean },
-      clientSummary: { type: Boolean },
-      clientAccount: { type: Boolean },
       provider: { type: Boolean },
-      providerSummary: { type: Boolean },
-      providerAccount: { type: Boolean },
       group: { type: Boolean },
-      field: { type: Boolean },
     },
-    content: {
-      resource: { type: Boolean },
-      gallery: { type: Boolean },
-    },
+    gallery: { type: Boolean },
     report: { type: Boolean },
     config: { type: Boolean },
   },
-  filterTransaction: {type: Boolean, default: false},
-  filterCompany: {type: Boolean, default: false},
-  transactionTypes: [{type: Schema.Types.ObjectId, ref: 'transaction-type'}],
-  editArticle: {type: Boolean, default: true },
-  allowDiscount: {type: Boolean, default: true },
-  allowPayment: {type: Boolean, default: true }
-})
+  filterTransaction: { type: Boolean, default: false },
+  filterCompany: { type: Boolean, default: false },
+  transactionTypes: [{ type: Schema.Types.ObjectId, ref: 'transaction-type' }],
+  editArticle: { type: Boolean, default: true },
+  allowDiscount: { type: Boolean, default: true },
+  allowPayment: { type: Boolean, default: true },
+});
 
-module.exports = PermissionSchema
+module.exports = PermissionSchema;
