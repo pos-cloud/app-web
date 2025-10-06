868b4944a8ec8aae8281651015b126b5d69429e2
jeremias vallejos <vallejosjeremias14@gmail.com>
2025-10-06 10:59:40 -0300

    style: clean code

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute } from '@angular/router';
 import { TranslateMePipe } from 'app/shared/pipes/translate-me';
 import { ApplicationService } from '../../core/services/application.service';
 import { MenuService } from '../../core/services/menu.service';
-import { Application, ApplicationType } from '../application/application.model';
 
 @Component({
   selector: 'app-menu',
@@ -15,16 +14,10 @@ import { Application, ApplicationType } from '../application/application.model';
 })
 export class MenuComponent implements OnInit {
   @ViewChild('menuSection') menuSection!: ElementRef;
-
   database: string;
   menu: [];
-  styleMenu: Application;
 
-  constructor(
-    private route: ActivatedRoute,
-    private _menu: MenuService,
-    private _objService: ApplicationService
-  ) {}
+  constructor(private route: ActivatedRoute, private _menu: MenuService, private _objService: ApplicationService) {}
 
   async ngOnInit() {
     this.route.params.subscribe((params) => {
@@ -32,7 +25,6 @@ export class MenuComponent implements OnInit {
     });
 
     this.getMenu();
-    //this.getApplication();
   }
 
   getMenu() {
@@ -71,44 +63,5 @@ export class MenuComponent implements OnInit {
 
   scrollToMenu() {
     this.menuSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  getApplication() {
-    this._objService
-      .getAll({
-        project: {
-          type: 1,
-          'menu.portain': 1,
-          'menu.background': 1,
-          'menu.article.font': 1,
-          'menu.article.size': 1,
-          'menu.article.color': 1,
-          'menu.article.style': 1,
-          'menu.article.weight': 1,
-          'menu.category.font': 1,
-          'menu.category.size': 1,
-          'menu.category.color': 1,
-          'menu.category.style': 1,
-          'menu.category.weight': 1,
-          'menu.price.font': 1,
-          'menu.price.size': 1,
-          'menu.price.color': 1,
-          'menu.price.style': 1,
-          'menu.price.weight': 1,
-          'menu.observation.font': 1,
-          'menu.observation.size': 1,
-          'menu.observation.color': 1,
-          'menu.observation.style': 1,
-          'menu.observation.weight': 1,
-        },
-        match: {
-          type: ApplicationType.Menu,
-        },
-      })
-      .subscribe((result) => {
-        // if (result.status === 200) {
-        // this.styleMenu = result.result[0]
-        // }
-      });
   }
 }
